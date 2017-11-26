6c99c2a10a0ca7b50e793859f984c2442abfe1bd
Tushar Chokshi <tushar.chokshi@cdk.com>
2017-11-25 22:12:01 -0800

    more

--- src/main/java/algorithms/graphmanipulation/BreathFirstSearchGrokkingAlgorithmBookWay.java
+++ src/main/java/algorithms/graphmanipulation/BreathFirstSearchGrokkingAlgorithmBookWay.java
@@ -1,55 +1,83 @@
 package algorithms.graphmanipulation;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-/**
- * @author Tushar Chokshi @ 11/25/17.
- *         <p>
- *         Grokking Algorithms book (Chapter 6)
- *         <p>
- *         Here, I am using Map to represent a graph.
+/*
+ * Grokking Algorithms book (Chapter 6)
+ * Here, I am using Map to represent a graph.
  */
 public class BreathFirstSearchGrokkingAlgorithmBookWay {
-    private static Map<Friend, Friend[]> friendsNetwork = new HashMap<>();
 
-    private static final Friend you;
-
-    static {
-        you = new Friend("Tushar", false);
-        Friend miral = new Friend("Miral", false);
-        Friend srikant = new Friend("Srikant", false);
+    private static Map<Friend, Friend[]> initialize() {
+        Friend you = new Friend("Tushar");
+        Friend miral = new Friend("Miral");
+        Friend srikant = new Friend("Srikant");
         Friend puja = new Friend("Puja", true);
-        Friend madhu = new Friend("Madhu", false);
-        Friend anoop = new Friend("Anoop", false);
-        Friend rakesh = new Friend("Rakesh", false);
-        Friend ronak = new Friend("Ronak", false);
+        Friend madhu = new Friend("Madhu");
+        Friend anoop = new Friend("Anoop");
+        Friend rakesh = new Friend("Rakesh");
+        Friend ronak = new Friend("Ronak");
+
+        Friend notConnectedPerson1 = new Friend("notConnectedPerson1");
+        Friend notConnectedPerson2 = new Friend("notConnectedPerson2");
 
         // create a graph using hash table
-        friendsNetwork.put(you, new Friend[]{miral, srikant, anoop, madhu, rakesh});
-        friendsNetwork.put(miral, new Friend[]{srikant, you, puja});
-        friendsNetwork.put(srikant, new Friend[]{you, miral, ronak});
+        Map<Friend, Friend[]> graph = new HashMap<>();
+        graph.put(you, new Friend[]{miral, srikant, anoop, madhu, rakesh});
+        graph.put(miral, new Friend[]{srikant, you, puja});
+        graph.put(srikant, new Friend[]{you, miral, ronak});
+
+        graph.put(notConnectedPerson1, new Friend[]{notConnectedPerson2});
+
+        return graph;
     }
 
 
     public static void main(String[] args) {
 
         {
+            Map<Friend, Friend[]> graph = initialize();
+
             Queue queue = new LinkedBlockingQueue();
 
             // Add the node from where you want start searching
-            queue.add(you);
+            queue.add(new Friend("Tushar"));
 
-            Friend mangoSellerFriend = searchMangoSellerFriendOfYours(queue);
+            List<Friend> visitedFriends = new LinkedList<>();
+            Friend mangoSellerFriend = searchMangoSellerFriendOfYours(graph, queue, visitedFriends);
 
-            System.out.println(mangoSellerFriend);// Friend{name='Puja'}
+            System.out.println("Found closest Mango Seller is: "+ mangoSellerFriend);// Friend{name='Puja'}
+        }
+
+        {
+            Map<Friend, Friend[]> graph = initialize();
+
+            Queue queue = new LinkedBlockingQueue();
+
+            // Add the node from where you want start searching
+            queue.add(new Friend("Tushar"));
+
+            List<Friend> visitedFriends = new LinkedList<>();
+            isThereAPathFromTusharToNotConnectedPerson1(graph, queue, visitedFriends);
+
+            if (!visitedFriends.contains(new Friend("notConnectedPerson1"))) {
+                System.out.println("notConnectedPerson1 is NOT connected to Tushar"); // this will be the output
+            } else {
+                System.out.println("notConnectedPerson1 is connected to Tushar");
+            }
         }
         {
+            Map<Friend, Friend[]> graph = initialize();
+
             Queue queue = new LinkedBlockingQueue();
 
             // Add the node from where you want to find the distance to another node
+            Friend you = new Friend("Tushar");
             queue.add(you);
 
             Map<Friend, Integer> friendDistanceMap = new HashMap<>();
@@ -57,44 +85,78 @@ public class BreathFirstSearchGrokkingAlgorithmBookWay {
             // distance from starting node to itself is 0
             friendDistanceMap.put(you, 0);
 
-            findShortestDistanceFromTusharToPuja(queue, friendDistanceMap);
+            Map<Friend, Integer> distances = findShortestDistances(graph, queue, friendDistanceMap);
+            System.out.println("distances from Tushar to other friends: " + distances);// {Friend{name='Srikant'}=1, Friend{name='Anoop'}=1, Friend{name='Madhu'}=1, Friend{name='Rakesh'}=1, Friend{name='Tushar'}=0, Friend{name='Puja'}=2, Friend{name='Ronak'}=2, Friend{name='Miral'}=1}
 
-            for (Friend friend : friendDistanceMap.keySet()) {
-                if (friend.getName().equalsIgnoreCase("Puja")) {
-                    System.out.println("Shortest distance from Tushar to Puja is " + friendDistanceMap.get(friend));//Shortest distance from Tushar to Puja is 2
-                }
-            }
+        }
+        {
+            Map<Friend, Friend[]> graph = initialize();
+
+            Queue queue = new LinkedBlockingQueue();
+
+            // Add the node from where you want to find the distance to another node
+            Friend you = new Friend("Tushar");
+            queue.add(you);
+
+            Map<Friend, Integer> friendDistanceMap = new HashMap<>();
+
+            // distance from starting node to itself is 0
+            friendDistanceMap.put(you, 0);
+
+            Integer distanceFromTusharToPuja = findShortestDistanceFromTusharToPuja(graph, queue, friendDistanceMap, new Friend("Puja"));
+            System.out.println("Shortest distance from Tushar to Puja: " + distanceFromTusharToPuja);
         }
     }
 
-    private static Friend searchMangoSellerFriendOfYours(Queue queue) {
+    private static Friend searchMangoSellerFriendOfYours(Map<Friend, Friend[]> graph, Queue queue, List<Friend> visitedFriends) {
         if (queue.isEmpty()) return null;
 
         Friend me = (Friend) queue.poll();
-        me.setVisited(true);
+        visitedFriends.add(me);
 
         if (me.isMangoSeller) return me;
 
-        Friend[] friends = friendsNetwork.get(me);
+        Friend[] friends = graph.get(me);
 
         // Add your closest friends to queue. This is needed to initiate the search.
         if (friends != null && friends.length > 0) {
             for (Friend friend : friends) {
-                if (!friend.isVisited()) {
+                if (!visitedFriends.contains(friend)) {
                     queue.add(friend);
                 }
             }
         }
 
-        return searchMangoSellerFriendOfYours(queue);
+        return searchMangoSellerFriendOfYours(graph, queue, visitedFriends);
     }
 
-    private static Map<Friend, Integer> findShortestDistanceFromTusharToPuja(Queue queue, Map<Friend, Integer> friendDistanceMap) {
+    private static Friend[] isThereAPathFromTusharToNotConnectedPerson1(Map<Friend, Friend[]> graph, Queue queue, List<Friend> visitedFriends) {
+        if (queue.isEmpty()) return null;
+
+        Friend me = (Friend) queue.poll();
+        visitedFriends.add(me);
+
+        Friend[] friends = graph.get(me);
+
+        // Add your closest friends to queue. This is needed to initiate the search.
+        if (friends != null && friends.length > 0) {
+            for (Friend friend : friends) {
+                if (!visitedFriends.contains(friend)) {
+                    queue.add(friend);
+                }
+            }
+        }
+
+        return isThereAPathFromTusharToNotConnectedPerson1(graph, queue, visitedFriends);
+
+    }
+
+    private static Map<Friend, Integer> findShortestDistances(Map<Friend, Friend[]> graph, Queue queue, Map<Friend, Integer> friendDistanceMap) {
         if (queue.isEmpty()) return friendDistanceMap;
 
         Friend me = (Friend) queue.poll();
 
-        Friend[] friends = friendsNetwork.get(me);
+        Friend[] friends = graph.get(me);
 
         // Add your closest friends to queue. This is needed to initiate the search.
         if (friends != null && friends.length > 0) {
@@ -110,8 +172,31 @@ public class BreathFirstSearchGrokkingAlgorithmBookWay {
             }
         }
 
-        return findShortestDistanceFromTusharToPuja(queue, friendDistanceMap);
+        return findShortestDistances(graph, queue, friendDistanceMap);
+    }
 
+    private static Integer findShortestDistanceFromTusharToPuja(Map<Friend, Friend[]> graph, Queue queue, Map<Friend, Integer> friendDistanceMap, Friend friendToFindDistanceTo) {
+        if (queue.isEmpty()) return 0;
+
+        Friend me = (Friend) queue.poll();
+
+        Friend[] friends = graph.get(me);
+
+        // Add your closest friends to queue. This is needed to initiate the search.
+        if (friends != null && friends.length > 0) {
+            for (Friend friend : friends) {
+                if (!friendDistanceMap.containsKey(friend)) {
+                    if (friend.equals(friendToFindDistanceTo)) {
+                        return friendDistanceMap.get(me) + 1;
+                    }
+                    friendDistanceMap.put(friend, friendDistanceMap.get(me) + 1);
+                    queue.add(friend);
+                }
+
+            }
+        }
+
+        return findShortestDistanceFromTusharToPuja(graph, queue, friendDistanceMap, friendToFindDistanceTo);
     }
 
     static class Friend { // vertex in graph
@@ -119,8 +204,12 @@ public class BreathFirstSearchGrokkingAlgorithmBookWay {
         private boolean isMangoSeller;
         private boolean visited; //most important variable
 
-        public Friend(String name, boolean isMangoSeller) {
+        public Friend(String name) {
             this.name = name;
+        }
+
+        public Friend(String name, boolean isMangoSeller) {
+            this(name);
             this.isMangoSeller = isMangoSeller;
         }
 
