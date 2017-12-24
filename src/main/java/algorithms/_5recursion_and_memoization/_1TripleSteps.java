a6bac13b651a86e2cf16479836ea5582adea2081
Tushar Chokshi <tushar.chokshi@cdk.com>
2017-12-23 21:44:10 -0800

    more

--- src/main/java/algorithms/_5recursion_and_memoization/_1TripleSteps.java
+++ src/main/java/algorithms/_5recursion_and_memoization/_1TripleSteps.java
@@ -8,6 +8,9 @@ Triple Steps:
 A child is running up a staircase with n steps and can hop either 1 step, 2 steps, or 3 steps at a time.
 Implement a method to count how many possible ways the child can run up the stairs.
 
+Watch 'Recurrence Relations Part2.mp4' video first.
+
+
 I have implemented this algorithm in multiple ways
 - Brute Force way
 - Top-Bottom Dynamic Programming (Memoization) way
--- src/main/java/algorithms/_5recursion_and_memoization/_2RobotInGrid.java
+++ src/main/java/algorithms/_5recursion_and_memoization/_2RobotInGrid.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 /*
+p.g. 345 of Cracking Coding Interview book
+
 Robot in a Grid:
 Imagine a robot sitting on the upper left corner of grid with r rows and c columns.
 The robot can only move in two directions, right and down, but certain cells are "off limits" such that
 the robot cannot step on them. Design an algorithm to find a path for the robot from the top left to the bottom right.
 
-p.g. 345 of Cracking Coding Interview book
+
+Watch 'Recurrence Relations Part2.mp4' video first.
 
 
                                     getPath(0,0,...)
