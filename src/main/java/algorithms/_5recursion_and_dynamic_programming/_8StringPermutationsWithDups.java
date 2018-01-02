23475051c8470657e5f2ccf1bf5edaccf900da8f
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-01-02 14:44:34 -0800

    more

--- src/main/java/algorithms/_5recursion_and_dynamic_programming/_8StringPermutationsWithDups.java
+++ src/main/java/algorithms/_5recursion_and_dynamic_programming/_8StringPermutationsWithDups.java
@@ -7,12 +7,142 @@ import java.util.Map;
 
 public class _8StringPermutationsWithDups {
     public static void main(String[] args) {
-        List<String> perms = getPerms("aabbbbc");
-        for (String perm : perms) {
-            System.out.println(perm);
+        String str = "aabc";
+        {
+            List<String> perms = getPerms(str);
+            for (String perm : perms) {
+                System.out.println(perm);
+            }
+        }
+        {
+            int level = 0;
+            char[] auxArray = new char[str.length()];
+            List<String> result = new ArrayList<>();
+
+
+            Map<Character, Integer> charCount = new HashMap<>();
+            char[] chars = str.toCharArray();
+            for (char c : chars) {
+                if (charCount.containsKey(c)) {
+                    charCount.put(c, charCount.get(c) + 1);
+                } else {
+                    charCount.put(c, 1);
+                }
+            }
+
+
+            char[] letters = new char[charCount.keySet().size()];
+            int[] count = new int[letters.length];
+            int c = 0;
+            for (Character ch : charCount.keySet()) {
+                letters[c] = ch;
+                count[c] = charCount.get(ch);
+                c++;
+            }
+
+
+            getPerms(str, level, count, letters, auxArray, result);
+
+            System.out.println(result);
+        }
+
+    }
+
+    /*
+        Watch 'String Permutation With Dups Algorithm.mp4'.
+
+        Unless you draw a recursion tree first, you won't be able code this algorithm.
+
+aux array
+-----------------
+| a | a | b | c |   - aabc
+-----------------
+          c   b     - aacb
+          and so on...
+
+
+                m("aabc", 0, count=[2 1 1],letters=[a b c], char[] auxArray, List<String> result)
+                |
+-----------------------------------------------------------------------------------------------------------------------------------
+|                                                                           |
+|                                           --------------------- for loop to find a letter having count > 0, a is found --------------------
+|                                           |                                                      |                                        |
+I/P:
+level=0                  reduce the count [1 1 1]     put letter 'a' in auxArray at level=0     m("aabc", 1, [1,1,1], [a,b,c], [a], []   restore the count back [2,1,1]
+letters=[a,b,c]                                       if(level=letter.length-1)                  |                                        (IMPORTANT)
+count  =[2,1,1]                                         then add auxArray into result list       |
+                                                                                                 |
+--------------------------------------------------------------------------------------------------
+|                                                                                                |
+|                                           --------------------- for loop to find a letter having count > 0, a is found --------------------               ---------------------b is found----------------------------------------------------------------------
+|                                           |                                               |                                               |               |                                                                                                   |
+level=1                             count=[0,1,1]       auxArray=[a,a]                  m("aabc", 2, [0 1 1], [a b c], [a,a], [])      count=[0,1,1]      count[0,0,1]      auxArray=[a,b,c,b]      m("aabc", 2, [0,0,1], [a,b,c], [a,b,c,b], [aabc,aacb])   count[0,1,1]
+letters=[a,b,c]                                                                             |                                                                                                                   |
+count  =[1,1,1]                                                                             |                                                                                                                   |
+                                                                                            |                                                                                                               .........
+---------------------------------------------------------------------------------------------
+|                                                                                           |
+|                                           --------------------- for loop to find a letter having count > 0, b is found ---------------------              ---------------------c is found------------------------------------------------------------------
+|                                           |                                               |                                                |              |                                                                                               |
+level=2                                 count=[0,0,1]        auxArray=[a,a,b]           m("aabc", 3, [0 0 1], [a b c], [a,a,b], [])     count=[0,1,1]     count[0,1,0]      auxArray=[a,a,c,c]        m("aabc", 3, [0,1,0], [a,b,c], [a,a,b], [aabc])   count[0,1,1]
+letters=[a,b,c]                                                                              |                                                                                                                  |
+count  =[0,1,1]                                                                              |                                                                                                                  ----------------------------------------------------------------
+                                                                                           1 |      2                                                                                                                                                                          |
+----------------------------------------------------------------------------------------------                                                                                                                                     ----------------------------------------------
+|                                                                                            |                                                                                                                                     |                                           |
+|                                           --------------------- for loop to find a letter having count > 0, c is found--------------------------------------------                                                               |
+|                                           |                                                      |                                                 |             |                                                             level=3                --------------------- for loop to find a letter having count > 0, b is found-------------------------------------
+level=3                                 count=[0,0,0]       auxArray=[a,a,b,c]                m("aabc", 4, [0 0 0], [a b c], [a,a,b,c], [aabc])  count=[0,0,1]  for loop will end so it will return                              letters=[a,b,c]        |                                                      |                                                        |
+letters=[a,b,c]                                             As level=letters.size()-1,                     |                                                                                                                     count  =[0,1,0]     count=[0,0,1]       auxArray=[a,a,c,b]                m("aabc", 4, [0 0 0], [a b c], [a,a,b,c], [aabc,aacb])  count=[0,1,1]
+count  =[0,0,1]                                                 it auxArray will be returned to result     |                                                                                                                     auxArray=[a,a,b,c]                      As level=letters.size()-1,
+                                                                                                           |                                                                                                                                                                it auxArray will be returned to result
+------------------------------------------------------------------------------------------------------------
+|                       |
+level=4                 for loop won't be able to find any letter with count>0
+letters=[a,b,c]         so it will return
+count  =[0,0,0]
+
+
+     */
+    private static void getPerms(String str, int level, int[] count, char[] letters, char[] auxArray, List<String> result) {
+
+        if (str == null || str.length() == 0) return;
+
+        if (count == null || count.length == 0) return;
+
+        if (letters == null || letters.length == 0) return;
+
+        System.out.println("entering level-"+level);
+
+        for (int i = 0; i < letters.length; i++) {
+            if (count[i] > 0) {
+                System.out.println("found letter with count > 0 - "+letters[i]+ " at level-"+level);
+
+                count[i]--;
+
+                auxArray[level] = letters[i];
+                System.out.println("writing "+letters[i]+ " at index-"+level +" in aux array");
+
+                getPerms(str, level + 1, count, letters, auxArray, result);
+
+                if (level == letters.length-1) {
+                    result.add(new String(auxArray));
+                }
+                count[i]++; // very important
+                System.out.println(letters[i]+"  level-"+level+"  count-"+convertIntArrayToString(count)+ "   aux array-"+new String(auxArray));
+            }
         }
     }
 
+    private static String convertIntArrayToString(int[] count) {
+        String str = "";
+        for (int c : count) {
+            str += c;
+        }
+        return str;
+    }
+
+    // From Cracking Coding Interview Book
     private static List<String> getPerms(String s) {
         List<String> result = new ArrayList<>();
         Map<Character, Integer> map = buildFreqTable(s);
