74250afeca495e191a6e5913371b3f81b793c624
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-01-04 21:49:47 -0800

    more

--- src/main/java/algorithms/_5recursion_and_dynamic_programming/_11CoinsChange.java
+++ src/main/java/algorithms/_5recursion_and_dynamic_programming/_11CoinsChange.java
@@ -70,8 +70,6 @@ public class _11CoinsChange {
     /*
 
         This algorithm is a good example of understanding how to start thinking Bottom-Up Dynamic programming right away instead of thinking about Recursion first.
-        This problem can be solved easily by thinking about Bottom-Up Dynamic approach rather than Recursion.
-
 
         Read Dynamic Programming from 'README_grokking_algorithms_book_notes.docx'
 
@@ -173,6 +171,7 @@ public class _11CoinsChange {
 
                 int coin = coins[row - 1];
 
+                // These formulas are important for bottom-up dynamic approach that you need to determine by drawing a matrix on paper and filling it up first before writing the code.
                 if (coin > col) {
                     memo[row][col] = memo[row - 1][col];
                 } else {
@@ -202,7 +201,155 @@ public class _11CoinsChange {
         return temp[coins.length][total];
     }
 
-    // This code is from Cracking In Coding Interview book that is hard for me to understand
+
+    /*
+        My thought process of how to work with 2-D problem
+        --------------------------------------------------
+
+        Coin Changing problem is 2-D because there are two dimensions - coins and amount.
+
+        Can I use Bottom-Up Dynamic Approach to start with instead of Brute-Force?
+
+            You can, if
+
+            - It is hard to think Brute-Force to begin with
+              If you can code Brute-Force, you can easily convert it into Top-Bottom Dynamic Approach.
+
+            - It is a 0/1 knapsack kind of problem
+              You knapsack(backpack) can hold some fixed amount of weight using various combinations of items available.
+              It means that your one dimension has to have fixed range AT LEAST. Here amount is fixed and you can use a same coin infinitely to make up an amount).
+
+        IMPORTANT:
+            Whether you start with Bottom-Up Dynamic Approach or Brute-Force Approach for knapsack kind of problem,
+            it is always better to draw a 2-D matrix on paper with one additional row and col (0th row and 0th col - it determines the most important exit condition) first.
+
+            For normal matrix traversal problem like EightQueens.java and RobotInGrid.java, matrix is provided as an input. You are not solving knapsack problem there.
+            So, you don't start matrix with additional row and col.
+
+        IMPORTANT:
+            If you choose to start with Bottom-Up Dynamic approach, first you need to fill up the matrix on paper starting from first cell.
+
+        IMPORTANT:
+            If you choose to go with Brute-Force (followed by Top-Bottom Dynamic Approach, if required),
+            then also you draw a matrix on paper. You may not need to fill it up.
+
+            When you think of recursion's reduce the problem by 1 approach, start from the last cell of the matrix (here amount=5 and coin=4)
+            Why?
+            Because normally, in 2-D problem, it is easier to think of a solution for the last element in drawn matrix instead of the first one.
+            Here, it is easier to think that value at cell(4,5) depends on the values from its previous cells.
+
+
+        IMPORTANT:
+            There are two dimensions of this problem (coins and amount).
+            So, you need to recurse this problem using both dimensions.
+
+        It means that you need to recurse this problem using in 2-D, if you are using Brute-Force.
+
+        There are 3 ways to solve 2-D problem:
+
+            1) Total Imperative Approach:
+
+              two loops (one loop for coins and another loop inside first one for amount)
+              This is basically 100% imperative approach and very hard to code using Brute-Force. It is used for Bottom-Up Dynamic approach.
+              You actually will not be able to use it for Brute-Force.
+
+            2) Half Imperative-Half Recursive Approach:  ----- This is the easiest approach among all. I would normally use this approach.
+
+               Use a loop for one of the dimension and use recursion for another dimension.
+
+               Use a look for amount and recursion for coins.
+
+                int count(int coins[], int coinsStartIndex, int coinsEndIndex, int amount) {
+                    ... exit conditions ...
+
+                    int lastCoin = coins[coinsEndIndex];
+
+                    for (int amt = amount; amt >= 0; amt--) { // loop for amount
+                        int totalWays = 0;
+
+                        // sometimes it is hard to find out all conditions to determine the value for one coin
+                        // Drawing matrix will help you. So remember to draw 2-D matrix for 2-D problems before you start writing the code.
+                        int waysUsingLastCoin = ..... // This value is determined based on conditions (comparisons between amt and coin)
+
+                        // recursion for coins
+                        int waysUsingPrevCoins = count(coins, coinsStartIndex, coinsEndIndex - 1, amt); // recursion for coins
+
+                        int totalWays = waysUsingLastCoin + waysUsingPrevCoins;
+                        return totalWays;
+                    }
+
+                }
+
+            3) Think of recursive approach for both dimensions (coins and amount).
+
+               This approach is a bit harder than approach 2 as per my experience. So, I normally go with approach 2.
+
+
+                int count(int coins[], int coinsStartIndex, int coinsEndIndex, int amount) {
+
+                    ... exit conditions ...
+
+                    int lastCoin = coins[coinsEndIndex];
+
+                    int waysUsingLastCoin = ...;
+
+                    // recurse using coins for the same amount
+                    int waysUsingPrevCoins = count_brute_force_two_level_recursion(coins, coinsStartIndex, coinsEndIndex - 1, amount);
+
+                    int totalWaysUsingLastCoinAndPrevCoins = waysUsingLastCoin + waysUsingPrevCoins;
+
+                    // In this algorithm, amount level recursion is happening in the condition to determine waysUsingLastCoin. So, you don't need additional reducing amount by 1 as shown in below statement.
+                    // But in other algorithms, you may need to do this and choose final return value using  totalWaysUsingLastCoinAndPrevCoins and totalWaysUsingCombinationsOfOtherCoinsAndAmount.
+
+                    // int totalWaysUsingCombinationsOfOtherCoinsAndAmount = count_brute_force_two_level_recursion(coins, coinsStartIndex, coinsEndIndex, amount - 1);
+                    // return (logic to determine final return value using totalWaysUsingLastCoinAndPrevCoins and totalWaysUsingCombinationsOfOtherCoinsAndAmount)
+
+                    return totalWaysUsingLastCoinAndPrevCoins;
+                }
+
+     */
+    private static int count_brute_force_my_way(int coins[], int coinsStartIndex, int coinsEndIndex, int amount) {
+
+        // if you don't have any coins, you cannot make any amount
+        if (coins == null || coins.length == 0) {
+            return 0;
+        }
+
+        // if you have coins, but target amount is 0, then there is only 1 way to make amount 0;
+        if (amount == 0) {
+            return 1; // this is a very important condition. If you keep it 0, then end result will be wrong. Best way to determine this value is 2-D matrix process of Bottom-Up Dynamic Approach. That's why you should think of Bottom-Up dynamic approach to begin with for this kind of problems.
+        }
+
+        if (coinsStartIndex > coinsEndIndex) {
+            return 0;
+        }
+
+        int lastCoin = coins[coinsEndIndex];
+
+        for (int amt = amount; amt >= 0; amt--) {// you can covert this for loop also in recursion, but this is easier to think. You can see an example of converting for loop into recursion in EightQueens.java
+            int totalWays = 0;
+
+            int waysUsingLastCoin = 0;
+            if (lastCoin == amt) {
+                waysUsingLastCoin = 1;
+            } else if (lastCoin > amt) {
+                waysUsingLastCoin = 0;
+            } else {
+                waysUsingLastCoin = count_brute_force_my_way(coins, coinsStartIndex, coinsEndIndex, amt - lastCoin);
+            }
+
+            int waysUsingPrevCoin = count_brute_force_my_way(coins, coinsStartIndex, coinsEndIndex - 1, amt);
+
+            totalWays = waysUsingLastCoin + waysUsingPrevCoin;
+            return totalWays;
+        }
+
+        return 0;
+    }
+
+    // This code is from Cracking In Coding Interview book that is hard for me to understand because code doesn't follow the rules that established for 2D problem.
+    // You should read explanation of the solution though.
+
     // Watch 'Coin Change - Determine Number of Ways to determine amount using different combinations of provided coins Using Top-Down Dynamic Programming approach.mp4'
     // Recursion
     private static int count_brute_force_book_way(int coins[], int coinsStartIndex, int coinsEndIndex, int amount) {
@@ -300,135 +447,6 @@ public class _11CoinsChange {
         }
 
         return ways;
-
-    }
-
-    /*
-        My thought process of how to work with 2-D problem
-        --------------------------------------------------
-
-        Can I use Bottom-Up Dynamic Approach to start with instead of Brute-Force?
-
-            You can, if
-
-            - It is hard to think Brute-Force to begin with
-              If you can code Brute-Force, you can easily convert it into Top-Bottom Dynamic Approach.
-
-            - It is a 0/1 knapsack kind of problem
-              You knapsack(backpack) can hold some fixed amount of weight using various combinations of items available.
-              It means that your one dimension has to have fixed range AT LEAST. Here amount is fixed and you can use a same coin infinitely to make up an amount).
-
-        IMPORTANT:
-        Whether you start with Bottom-Up Dynamic Approach or Brute-Force, it is always better to draw a 2-D matrix on paper with one additional row and col (0th row and 0th col - it determines the most important exit condition).
-
-        IMPORTANT:
-        If you choose to go with Brute-Force (followed by Top-Bottom Dynamic Approach, if required),
-        then you when you think of recursion's reduce the problem by 1. Start from the last cell of the matrix (here amount=5 and coin=4)
-        Why?
-        Because normally, in 2-D problem, it is easier to think of a solution to solve the problem of nth element using solution of a problem of 1 to n-1 elements.
-        Here, it is easier to think that value at cell(4,5) depends on the values from its previous cells.
-
-
-        There are two dimensions of this problem (coins and amount).
-        So, you need to recurse this problem using both dimensions.
-
-        It means that you need to recurse this problem using in 2-D, if you are using Brute-Force.
-
-        There are 3 ways to solve 2-D problem:
-
-            1) Total Imperative Approach:
-
-              two loops (one loop for coins and another loop inside first one for amount)
-              This is basically 100% imperative approach and very hard to code. So avoid this approach
-
-            2) Half Imperative Approach:  ----- This is the easiest approach among all. I would normally use this approach.
-
-               Think of recursive approach for coins and loop for amount.
-
-                int count(int coins[], int coinsStartIndex, int coinsEndIndex, int amount) {
-                    ... exit conditions ...
-
-                    int lastCoin = coins[coinsEndIndex];
-
-                    for (int amt = amount; amt >= 0; amt--) { // loop for amount
-                        int totalWays = 0;
-
-                        // sometimes it is hard to find out all conditions to determine the value for one coin
-                        // Drawing matrix will help you. So remember to draw 2-D matrix for 2-D problems before you start writing the code.
-                        int waysUsingLastCoin = ..... // This value is determined based on conditions (comparisons between amt and coin)
-
-                        // recursion for coins
-                        int waysUsingPrevCoins = count(coins, coinsStartIndex, coinsEndIndex - 1, amt); // recursion for coins
-
-                        int totalWays = waysUsingLastCoin + waysUsingPrevCoins;
-                        return totalWays;
-                    }
-
-                }
-
-            3) Think of recursive approach for both coins and amount.
-               This approach is a bit harder than approach 2 as per my experience. So, I normally go with approach 2.
-
-                int count(int coins[], int coinsStartIndex, int coinsEndIndex, int amount) {
-
-                    ... exit conditions ...
-
-                    int lastCoin = coins[coinsEndIndex];
-
-                    int waysUsingLastCoin = ...;
-
-                    // recurse using coins for the same amount
-                    int waysUsingPrevCoins = count_brute_force_two_level_recursion(coins, coinsStartIndex, coinsEndIndex - 1, amount);
-
-                    int totalWaysUsingLastCoinAndPrevCoins = waysUsingLastCoin + waysUsingPrevCoins;
-
-                    // In this algorithm, amount level recursion is happening in the condition to determine waysUsingLastCoin. So, you don't need additional reducing amount by 1 as shown in below statement.
-                    // But in other algorithms, you may need to do this and choose final return value using  totalWaysUsingLastCoinAndPrevCoins and totalWaysUsingCombinationsOfOtherCoinsAndAmount.
-
-                    // int totalWaysUsingCombinationsOfOtherCoinsAndAmount = count_brute_force_two_level_recursion(coins, coinsStartIndex, coinsEndIndex, amount - 1);
-                    // return (logic to determine final return value using totalWaysUsingLastCoinAndPrevCoins and totalWaysUsingCombinationsOfOtherCoinsAndAmount)
-
-                    return totalWaysUsingLastCoinAndPrevCoins;
-                }
-
-     */
-    private static int count_brute_force_my_way(int coins[], int coinsStartIndex, int coinsEndIndex, int amount) {
-
-        // if you don't have any coins, you cannot make any amount
-        if (coins == null || coins.length == 0) {
-            return 0;
-        }
-
-        // if you have coins, but target amount is 0, then there is only 1 way to make amount 0;
-        if (amount == 0) {
-            return 1; // this is a very important condition. If you keep it 0, then end result will be wrong. Best way to determine this value is 2-D matrix process of Bottom-Up Dynamic Approach. That's why you should think of Bottom-Up dynamic approach to begin with for this kind of problems.
-        }
-
-        if (coinsStartIndex > coinsEndIndex) {
-            return 0;
-        }
-
-        int lastCoin = coins[coinsEndIndex];
-
-        for (int amt = amount; amt >= 0; amt--) {// you can covert this for loop also in recursion, but this is easier to think. You can see an example of converting for loop into recursion in EightQueens.java
-            int totalWays = 0;
-
-            int waysUsingLastCoin = 0;
-            if (lastCoin == amt) {
-                waysUsingLastCoin = 1;
-            } else if (lastCoin > amt) {
-                waysUsingLastCoin = 0;
-            } else {
-                waysUsingLastCoin = count_brute_force_my_way(coins, coinsStartIndex, coinsEndIndex, amt - lastCoin);
-            }
-
-            int waysUsingPrevCoin = count_brute_force_my_way(coins, coinsStartIndex, coinsEndIndex - 1, amt);
-
-            totalWays = waysUsingLastCoin + waysUsingPrevCoin;
-            return totalWays;
-        }
-
-        return 0;
 
     }
 
