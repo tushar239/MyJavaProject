ca1e26e3c124b7318da45940ee0bb98934420b14
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-01-11 10:42:01 -0800

    more

--- src/main/java/algorithms/_6sort__search_merge/substring_search/FindSubstringFromString.java
+++ src/main/java/algorithms/_6sort__search_merge/substring_search/FindSubstringFromString.java
@@ -13,6 +13,9 @@ package algorithms._6sort__search_merge.substring_search;
     https://www.youtube.com/watch?v=G-h1Dph9IOE
 
 
+    The Rabin-Karp algorithm
+    https://www.youtube.com/watch?v=oxd_Z1osgCk
+
     Brute Force Algorithm is not just for strings. It is applicable to array and tree also.
     e.g. FindWhetherTreeIsBalanced.java, PathWithSums.java
  */
