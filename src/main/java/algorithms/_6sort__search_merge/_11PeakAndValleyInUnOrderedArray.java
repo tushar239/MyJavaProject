c234e4de71a4eb09ec6b160c8e535d020cd1b706
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-01-15 17:34:27 -0800

    more

--- src/main/java/algorithms/_6sort__search_merge/_11PeakAndValleyInUnOrderedArray.java
+++ src/main/java/algorithms/_6sort__search_merge/_11PeakAndValleyInUnOrderedArray.java
@@ -5,6 +5,8 @@ Find Peak from UnOrdered array:
 This is not from Cracking Coding Interview Book, but it is important to understand this algorithm before understanding SortArrayIntoAlternativeSequencesOfPeaksAndVallleys.java
 
 https://www.youtube.com/watch?v=a7D77DdhlFc
+
+Binary Search can be used to find peak/valley.
  */
 public class _11PeakAndValleyInUnOrderedArray {
 }
