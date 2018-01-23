17c5d85955bd88da16b7ab39c690802b8babd6f5
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-01-23 15:18:34 -0800

    more

--- src/main/java/algorithms/_7bitwise_operators/_8DrawLine.java
+++ src/main/java/algorithms/_7bitwise_operators/_8DrawLine.java
@@ -1,64 +1,13 @@
 package algorithms._7bitwise_operators;
 
+/*
+Draw a line:
+
+http://www.goldsborough.me/bits/c++/low-level/problems/2015/10/11/23-52-02-bit_manipulation/#drawing-a-line-in-a-monochrome-screen
+ */
 public class _8DrawLine {
-    private static void draw_line(char screen[],
-                                  int width,
-                                  int x_1,
-                                  int x_2,
-                                  int y) {
-        if (x_1 == x_2) return;
-
-        // First byte of the row
-        int first_byte = width * y;
-
-        // The byte at which we'll start the line
-        int lower_byte = first_byte + (x_1 / 8);
-
-        // The byte at which we'll end the line
-        int upper_byte = first_byte + (x_2 / 8);
-
-        // The bit in the lower byte
-        x_1 %= 8;
-
-        // The bit in the upper byte
-        x_2 %= 8;
-
-        // Special case when the bits are in the same byte
-        if (lower_byte == upper_byte) {
-            // Create a mask with the appropriate number of bits
-            // and shift them into place. Note that x_2 is exclusive.
-            int i = ((1 << (x_2 - x_1)) - 1) << (8 - x_2);
-            screen[lower_byte] = (char) i;
-        } else {
-            screen[lower_byte] = (char) (0xFF >> x_1);
-
-            screen[upper_byte] = (char) (0xFF << (8 - x_2));
-
-            for (++lower_byte; lower_byte < upper_byte; ++lower_byte) {
-                screen[lower_byte] = 0xFF;
-            }
-        }
-
-    }
 
     public static void main(String[] args) {
-
-        //System.out.println(1/8);//0
-
-        /*char screen[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-        draw_line(screen, 4, 1, 3, 3);
-
-        // Representation
-        for (int i = 0; i < 16; ++i) {
-            if (i % 4 == 0) System.out.println();
-
-            System.out.print(screen[i]);
-
-            if ((i + 1) % 4 != 0) System.out.print("|");
-        }*/
-
-
         // screen with
         // 4 rows (0 to 3)
         // each row having 8 bytes. so width of a screen is 64 bits (64 pixels)
@@ -88,7 +37,7 @@ public class _8DrawLine {
 // startPixel = 10
 // endPixel = 20
     private static void drawLine(byte[] screen, int width, int startPixel, int endPixel, int row) {
-        if(endPixel < startPixel) return;
+        if (endPixel < startPixel) return;
 
         int widthOfScreenInBytes = width / 8; // number of bytes in one row. e.g. if width=64 bits/pixels, then number of bytes in one row=64/8=8. It means that first 8 elements of screen array will be in row 0, second 8 elements will be in row 1 and so on
 
@@ -108,7 +57,7 @@ public class _8DrawLine {
         if (end_byte_of_line < start_byte_of_line) return;
 
         for (int i = start_byte_of_line; i <= end_byte_of_line; i++) {
-            screen[i] = (byte) 0xFF;// is same as (byte)-1
+            screen[i] = (byte) 0xFF;// is same as (byte)-1,
         }
 
         //System.out.println(BitwiseManipulationUtil.convertUnsignedIntToBinary(0xFF));// 1111 1111
