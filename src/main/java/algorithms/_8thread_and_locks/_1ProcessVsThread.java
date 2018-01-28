89fe3342304ac5d052c9f29b6b2b473209111b5e
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-01-27 17:34:34 -0800

    more

--- src/main/java/algorithms/_8thread_and_locks/_1ProcessVsThread.java
+++ src/main/java/algorithms/_8thread_and_locks/_1ProcessVsThread.java
@@ -15,63 +15,64 @@ When one thread modifies a process resource, the change is immediately visible t
 
 
 Program vs Process:
-https://www.youtube.com/watch?v=ucVm_arB-fw
+    https://www.youtube.com/watch?v=ucVm_arB-fw
 
 Process vs Thread:
-https://www.youtube.com/watch?v=brdp6d6KDso
-
+    https://www.youtube.com/watch?v=brdp6d6KDso
 
 States in the process, how they are moved from one to another state using Schedulers and what is Context Switching:
-https://www.youtube.com/watch?v=ucVm_arB-fw
+
+    https://www.youtube.com/watch?v=ucVm_arB-fw
+    https://www.youtube.com/watch?v=e8YxIdwodB8
 
 
-Process State Diagram:
+    Process State Diagram:
 
-        LTS                     STS/dispatcher
-    new state ---> ready state ------------------> running state -> termination state
-                   ^ | ^    ^                          |   |
-                   | |  |   |    priority              |   |
-               ----- |  |   ---------------------------    |
-               |     |  |       time quantum               |
-               |     |  |                                  |
-             r |  s  |  |                                  |
-             e |  u  |  ----waiting for i/o state ----------
-             s |  s  |         ^       |
-             u |  p  |       r |     s |  MTS does
-             m |  e  |       e |     u |  this job
-             e |  n  |       s |     s |
-               |  d  |       u |     p |
-               |     |       m |     e |
-               |     |       e |     n |
-               |     v         |     d v
-    suspend ready state <----  suspend wait state
+            LTS                     STS/dispatcher
+        new state ---> ready state ------------------> running state -> termination state
+                       ^ | ^    ^                          |   |
+                       | |  |   |    priority              |   |
+                   ----- |  |   ---------------------------    |
+                   |     |  |       time quantum               |
+                   |     |  |                                  |
+                 r |  s  |  |                                  |
+                 e |  u  |  ----waiting for i/o state ----------
+                 s |  s  |         ^       |
+                 u |  p  |       r |     s |  MTS does
+                 m |  e  |       e |     u |  this job
+                 e |  n  |       s |     s |
+                   |  d  |       u |     p |
+                   |     |       m |     e |
+                   |     |       e |     n |
+                   |     v         |     d v
+        suspend ready state <----  suspend wait state
 
-Processes are of two types - CPU bound  (it requires more CPU to complete)
-                           - I/O bound  (it has more I/O operations. For I/O operations, process is taken out from 'running' state to 'waiting for i/o' state. once i/o operation is done, it is brought back to 'ready' state, not running state.)
+    Processes are of two types - CPU bound  (it requires more CPU to complete)
+                               - I/O bound  (it has more I/O operations. For I/O operations, process is taken out from 'running' state to 'waiting for i/o' state. once i/o operation is done, it is brought back to 'ready' state, not running state.)
 
-LTS (Long Term Scheduler) decides number of processes to be in ready state. It should powerful because it should decide the mixture of CPU bound and I/O bound processes.
-If there are many I/O bound processes, then CPU will be idle for a long time.
-If there are many CPU bound processes, then CPU will be busy for a long time.
-So, Long Term Scheduler should be smart enough to decide the mixture of both.
+    LTS (Long Term Scheduler) decides number of processes to be in ready state. It should powerful because it should decide the mixture of CPU bound and I/O bound processes.
+    If there are many I/O bound processes, then CPU will be idle for a long time.
+    If there are many CPU bound processes, then CPU will be busy for a long time.
+    So, Long Term Scheduler should be smart enough to decide the mixture of both.
 
 
-STS(Short Term Scheduler) picks up a process from ready state and moves it to running state.
-Dispatcher creates a PCB (process control block) that holds information about that process.
+    STS(Short Term Scheduler) picks up a process from ready state and moves it to running state.
+    Dispatcher creates a PCB (process control block) that holds information about that process.
 
-PCB has properties of a process
-- process id
-- priority
-- process state
-- general purpose registers
-- list of open files
-- list of open devices
-- protection
+    PCB has properties of a process
+    - process id
+    - priority
+    - process state
+    - general purpose registers
+    - list of open files
+    - list of open devices
+    - protection
 
-PCBs of processes are connected to each other in a linked list.
+    PCBs of processes are connected to each other in a linked list.
 
-MTS(Medium Term Scheduler) decides when to suspend and resume a process. When higher priority process comes, MTS has to move the process form RAM to HDD and empty resources for higher priority process.
-This is called CONTEXT SWITCHING. Context Switching is expensive because it has to save entire context of a process that needs to be suspended and restore the context of a process that needs to be run from RAM to HDD and vice-a-versa.
-I/O can continue to happen in suspended state also (in HDD also).
+    MTS(Medium Term Scheduler) decides when to suspend and resume a process. When higher priority process comes, MTS has to move the process form RAM to HDD and empty resources for higher priority process.
+    This is called CONTEXT SWITCHING. Context Switching is expensive because it has to save entire context of a process that needs to be suspended and restore the context of a process that needs to be run from RAM to HDD and vice-a-versa.
+    I/O can continue to happen in suspended state also (in HDD also).
 
 */
 
