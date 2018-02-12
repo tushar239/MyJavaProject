4ebd6a93820eb0e3e6f67681509e8d0b4923f920
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-02-11 22:44:06 -0800

    more

--- src/main/java/algorithms/interviewbit/array/MaximumAbsoluteDifference.java
+++ src/main/java/algorithms/interviewbit/array/MaximumAbsoluteDifference.java
@@ -16,19 +16,27 @@ f(2, 3) = f(3, 2) = |3 - (-1)| + |2 - 3| = 5
 
 So, we return 5.
 
-
-To solve this problem, you need to first learn MaximumDifferenceBetweenTwoElementsOfAnArray.java
+You have to memorize the solution. It's a very tricky problem.
 */
 public class MaximumAbsoluteDifference {
 
     public static void main(String[] args) {
         MaximumAbsoluteDifference instance = new MaximumAbsoluteDifference();
-        int A[] = {1, 3, -1};
-        //int A[] = {2, 3, 10, 6, 4, 8, 1};
+        //int A[] = {1, 3, -1}; // 5
+        //int A[] = {2, 3, 10, 6, 4, 8, 1};//13
+        //int A[] = { 89, -83, 38, 58, 79, -80, 8, 19, 31, -95 };//193
+        //int A[] = {2, 2, 2};// 0 (expected value is 2)
+        int A[] = {55, -8, 43, 52, 8, 59, -91, -79, -18, -94};// 157 (expected result=158)
         int maxSum = instance.maxAbsoluteDifference(A);
+        System.out.println(maxSum);
+        maxSum = instance.maxAbsoluteDifference2(A);
         System.out.println(maxSum);
     }
 
+    /*
+    I thought that I can apply the same logic as MaximumDifferenceBetweenTwoElementsOfAnArray.java here
+    But that won't work because absolute_diff_between_indices(i-j) does not necessarily give max value when absolute_diff_between_elements(A[i]-A[j]) gives max value.
+    */
     private int maxAbsoluteDifference(int A[]) {
         if (A == null || A.length == 0) return 0;
 
@@ -80,6 +88,82 @@ public class MaximumAbsoluteDifference {
         System.out.println("max_element: " + max_element);
 
         return max_sum_of_two_diffs;
+
+    }
+
+    /*
+    https://www.geeksforgeeks.org/maximum-absolute-difference-value-index-sums/
+
+    f(i, j) = |A[i] – A[j]| + |i – j| can be written in 4 ways (Since we are looking at max value, we don’t even care if the value becomes negative as long as we are also covering the max value in some way).
+
+    Case 1:
+        A[i] > A[j] and i > j
+        |A[i] - A[j]| = A[i] - A[j]
+        |i -j| = i - j
+
+        hence, f(i, j) = (A[i] + i) - (A[j] + j)
+
+    Case 2:
+        A[i] < A[j] and i < j
+        |A[i] - A[j]| = -(A[i]) + A[j]
+        |i -j| = -(i) + j
+
+        hence, f(i, j) = -(A[i] + i) + (A[j] + j)  = (A[j] + j) - (A[i] + i)
+
+    Case 3:
+        A[i] > A[j] and i < j
+        |A[i] - A[j]| = A[i] - A[j]
+        |i -j| = -(i) + j
+
+        hence, f(i, j) = (A[i] - i) - (A[j] - j)
+
+    Case 4:
+        A[i] < A[j] and i > j
+        |A[i] - A[j]| = -(A[i]) + A[j]
+        |i -j| = i - j
+
+        hence, f(i, j) = -(A[i] - i) + (A[j] - j) = (A[j] - j) - (A[i] - i)
+
+
+    Note that case 1 and 2 are equivalent and so are case 3 and 4
+    and hence we can design our algorithm only for two cases as it will cover all the possible cases.
+
+    So, now we have two cases
+
+    f(i, j) = (A[i] + i) - (A[j] + j)
+    f(i, j) = (A[i] - i) - (A[j] - j)
+
+    i and j are just indices in an array.
+    Basically, it means that (A[j] + j) is same as min value of (A[i] + i)
+    similarly, (A[j] - j) is same as min value of (A[i] - i)
+
+    So, keep track of min and max values of (A[i] + i) and (A[i] - i)
+    and get the values of above two cases
+    and find max of above two cases' results.
+
+     */
+    private int maxAbsoluteDifference2(int A[]) {
+        if (A == null || A.length == 0) return 0;
+
+        int ans = 0;
+
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+
+        for (int i = 0; i < A.length; i++) {
+            max1 = Math.max(max1, A[i] + i);
+            min1 = Math.min(min1, A[i] + i);
+
+            max2 = Math.max(max2, A[i] - i);
+            min2 = Math.min(min2, A[i] - i);
+        }
+        int case1Result = max1 - min1;
+        int case3Result = max2 - min2;
+
+        ans = Math.max(ans, case1Result);
+        ans = Math.max(ans, case3Result);
+
+        return ans;
 
     }
 }
