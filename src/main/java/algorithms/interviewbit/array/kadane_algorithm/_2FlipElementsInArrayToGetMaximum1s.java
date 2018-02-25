601226299892707d5f1d94218abdf69d4a746772
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-02-24 23:25:58 -0800

    more

--- src/main/java/algorithms/interviewbit/array/kadane_algorithm/_2FlipElementsInArrayToGetMaximum1s.java
+++ src/main/java/algorithms/interviewbit/array/kadane_algorithm/_2FlipElementsInArrayToGetMaximum1s.java
@@ -1,5 +1,38 @@
 package algorithms.interviewbit.array.kadane_algorithm;
 
+/*
+Flip elements in array to get maximum 1s:
+
+http://edusagar.com/questions/arrays/flip-elements-in-array-to-get-maximum-1s
+
+You are given a binary string(i.e. with characters 0 and 1) S consisting of characters S1, S2, …, SN. In a single operation, you can choose two indices L and R such that 1 ≤ L ≤ R ≤ N and flip the characters SL, SL+1, …, SR. By flipping, we mean change character 0 to 1 and vice-versa.
+
+Your aim is to perform ATMOST one operation such that in final string number of 1s is maximised. If you don’t want to perform the operation, return an empty array. Else, return an array consisting of two elements denoting L and R. If there are multiple solutions, return the lexicographically smallest pair of L and R.
+Notes: Pair (a, b) is lexicographically smaller than pair (c, d) if a < c or, if a == c and b < d
+
+For example,
+
+  S = 010
+
+Pair of [L, R] | Final string
+_______________|_____________
+[1 1]          | 110
+[1 2]          | 100
+[1 3]          | 101
+[2 2]          | 000
+[2 3]          | 001
+
+We see that two pairs [1, 1] and [1, 3] give same number of 1s in final string. So, we return [1, 1].
+Another example,
+
+ If S = 111
+
+No operation can give us more than three 1s in final string. So, we return empty array [].
+
+
+Solution:
+You need to use Kadane's algorithm (See MaxSumContiguousSubarray.java)
+*/
 public class _2FlipElementsInArrayToGetMaximum1s {
 
     public static void main(String[] args) {
@@ -53,6 +86,11 @@ public class _2FlipElementsInArrayToGetMaximum1s {
 
         for (int i = startIndex; i < A.length; i++) {
             int element = A[i];
+            // Important:
+            // In MaxSumContiguousSubArray.java, you were adding actual element to sum.
+            // Here, you need to consider 0 as 1 and 1 as -1 to add it in sum.
+            // basically, you increment the sum by 1, if you find element==0
+            // otherwise, you decrement the sum by 1.
             if (element == 0) {
                 sum += 1;
             } else {
