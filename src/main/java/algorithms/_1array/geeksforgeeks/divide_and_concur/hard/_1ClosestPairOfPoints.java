f8d95561d4bbc1ee55166b156693e9e6e52f3850
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-03-11 21:54:43 -0700

    more

---
+++ src/main/java/algorithms/_1array/geeksforgeeks/divide_and_concur/hard/_1ClosestPairOfPoints.java
@@ -0,0 +1,22 @@
+package algorithms._1array.geeksforgeeks.divide_and_concur.hard;
+
+/*
+Closest Pair of Points | O(n (log n)^2) Implementation
+https://www.geeksforgeeks.org/closest-pair-of-points/
+
+Closest Pair of Points | O(n log n) Implementation
+https://www.geeksforgeeks.org/closest-pair-of-points-onlogn-implementation/
+
+Youtube video:
+https://www.youtube.com/watch?v=6UBDkbVhJck
+
+
+This algorithm is hard because it is related to points scattered in 2-D plane.
+
+IMPORTANT:
+This algorithm is extremely important to understand 'divide & concur' concept and 'Back Tracking' strategy to find time complexity.
+
+*/
+
+public class _1ClosestPairOfPoints {
+}
