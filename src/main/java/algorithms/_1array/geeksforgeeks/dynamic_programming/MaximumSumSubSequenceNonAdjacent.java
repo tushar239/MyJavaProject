5226e588455b200d2274d78d514632b1109585f0
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-03-13 15:57:27 -0700

    more

--- src/main/java/algorithms/_1array/geeksforgeeks/dynamic_programming/MaximumSumSubSequenceNonAdjacent.java
+++ src/main/java/algorithms/_1array/geeksforgeeks/dynamic_programming/MaximumSumSubSequenceNonAdjacent.java
@@ -22,10 +22,23 @@ Examples :
 
 Solution:
 
-    This algorithm is based on Dynamic Programming and you need to MEMORIZE the approach.
+    I would say that this algorithm is PARTIALLY using  Dynamic Programming approach because
+    it looks like NP-complete problem
+    +
+    it needs to MEMORIZE the previous values to calculate current value .
 
-    What is Dynamic Programming?
-    If you use previously calculated result to calculate current result, you are using dynamic programming.
+    But it is not either using the recursion or not trying to avoid it.
+
+    When to use Dynamic Programming?
+    - You are trying to figure out n! kind of problem
+    - If you use previously calculated result to calculate current result, you are using dynamic programming.
+
+        Dynamic Programming can be done in two ways
+
+        - Top-Bottom approach (it memoizes previously calculated values, but doesn't avoid recursion)
+        - Bottom-Up approach (it memoizes previously calculated values and also avoids recursion)
+          This requires you to build a memoization table.
+          You can create memoization table only if you know the max value (max weight that your knapsack can handle/max sum that you need to achieve etc.)
 
     Watch this video to understand the algorithm
     https://www.youtube.com/watch?v=UtGtF6nc35g
@@ -37,6 +50,9 @@ public class MaximumSumSubSequenceNonAdjacent {
         int A[] = {4, 1, 1, 4, 2, 1};
         int maxSum = findMaxSum(A);
         System.out.println(maxSum);//9 = 4+4+1
+
+        Use maxSum_recursion = findMaxSum_recursion(A, 0, A.length - 1);
+        System.out.println(maxSum_recursion.maxSum);
     }
 
     private static int findMaxSum(int[] A) {
@@ -57,5 +73,52 @@ public class MaximumSumSubSequenceNonAdjacent {
         }
 
         return Math.max(include, exclude);
+    }
+
+    private static class Use {
+        private boolean firstElementUsed;
+        private int maxSum;
+
+        public Use(boolean firstElementUsed, int maxSum) {
+            this.firstElementUsed = firstElementUsed;
+            this.maxSum = maxSum;
+        }
+
+        public boolean isFirstElementUsed() {
+            return firstElementUsed;
+        }
+
+        public int getMaxSum() {
+            return maxSum;
+        }
+    }
+
+    private static Use findMaxSum_recursion(int[] A, int start, int end) {
+        //if (A == null || (start > end)) return new Use(false, 0);
+        if (start == end) return new Use(true, 0);
+        if ((end - start) == 1) {
+            int max = Math.max(A[start], A[end]);
+            if(max== A[start]) {
+                return new Use(true, A[start]);
+            }
+            return new Use(false, A[end]);
+        }
+        if ((end - start) == 2) {
+            if(A[start]+A[end] >= A[start+1]) {
+                return new Use(true, A[start]+A[end]);
+            }
+            return new Use(false, A[start+1]);
+        }
+
+        int first = A[start];
+        Use used = findMaxSum_recursion(A, start + 1, end);
+        int maxSumFromRemainingArray = used.maxSum;
+        if(!used.firstElementUsed) {
+            if(maxSumFromRemainingArray+first > maxSumFromRemainingArray) {
+                return new Use(true, maxSumFromRemainingArray+first);
+            }
+            return new Use(false, maxSumFromRemainingArray);
+        }
+        return new Use(false, maxSumFromRemainingArray);
     }
 }
