f5a3939b3029e139b8ad8fc803d8cc3f701fc66a
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-03-24 17:45:29 -0700

    more

--- src/main/java/algorithms/_1array/geeksforgeeks/dynamic_programming/_4LongestCommonSubSequence.java
+++ src/main/java/algorithms/_1array/geeksforgeeks/dynamic_programming/_4LongestCommonSubSequence.java
@@ -69,9 +69,9 @@ public class _4LongestCommonSubSequence {
 
             System.out.println("Brute Force: " + "S1=" + s1 + ", S2=" + s2);
             System.out.println();
-            int lca = LCA_Brute_Force(s1.toCharArray(), s2.toCharArray(), 0, s1.length() - 1, 0, s2.length() - 1);
+            int lcs = LCS_Brute_Force(s1.toCharArray(), s2.toCharArray(), 0, s1.length() - 1, 0, s2.length() - 1);
             System.out.println();
-            System.out.println("Max common subsequence: " + lca);//3
+            System.out.println("Max common subsequence: " + lcs);//3
 
             System.out.println();
             System.out.println();
@@ -79,9 +79,9 @@ public class _4LongestCommonSubSequence {
 
             System.out.println("Top-Down Dynamic Programming: " + "S1=" + s1 + ", S2=" + s2);
             System.out.println();
-            lca = LCA_Top_Down_Approach(s1.toCharArray(), s2.toCharArray(), 0, s1.length() - 1, 0, s2.length() - 1, new HashMap<>());
+            lcs = LCS_Top_Down_Approach(s1.toCharArray(), s2.toCharArray(), 0, s1.length() - 1, 0, s2.length() - 1, new HashMap<>());
             System.out.println();
-            System.out.println("Max common subsequence: " + lca);//3
+            System.out.println("Max common subsequence: " + lcs);//3
         }
         System.out.println();
         System.out.println();
@@ -92,7 +92,7 @@ public class _4LongestCommonSubSequence {
 
             System.out.println("Brute Force: " + "S1=" + s1 + ", S2=" + s2);
             System.out.println();
-            int lca = LCA_Brute_Force(s1.toCharArray(), s2.toCharArray(), 0, s1.length() - 1, 0, s2.length() - 1);
+            int lca = LCS_Brute_Force(s1.toCharArray(), s2.toCharArray(), 0, s1.length() - 1, 0, s2.length() - 1);
             System.out.println("Max common subsequence: " + lca);//0
             System.out.println();
             System.out.println("Total comparisons: " + cnt);// approx O(2^(m+n)) = O(2^(5+4))
@@ -103,7 +103,7 @@ public class _4LongestCommonSubSequence {
 
             System.out.println("Top-Down Dynamic Programming: " + "S1=" + s1 + ", S2=" + s2);
             System.out.println();
-            lca = LCA_Top_Down_Approach(s1.toCharArray(), s2.toCharArray(), 0, s1.length() - 1, 0, s2.length() - 1, new HashMap<>());
+            lca = LCS_Top_Down_Approach(s1.toCharArray(), s2.toCharArray(), 0, s1.length() - 1, 0, s2.length() - 1, new HashMap<>());
             System.out.println("Max common subsequence: " + lca);//0
             System.out.println();
             System.out.println("Total comparisons: " + count);// approx O( (m+n)^2 ) = O( (5+4)^2 )
@@ -112,7 +112,7 @@ public class _4LongestCommonSubSequence {
 
     static int cnt = 0;
 
-    private static int LCA_Brute_Force(char[] S1, char[] S2, int s1Start, int s1End, int s2Start, int s2End) {
+    private static int LCS_Brute_Force(char[] S1, char[] S2, int s1Start, int s1End, int s2Start, int s2End) {
         cnt++;
         if (s1End < s1Start || s2End < s2Start) return 0;
 
@@ -123,17 +123,17 @@ public class _4LongestCommonSubSequence {
         // if both chars matches, then reduce both strings' end index by 1
         if (s1Char == s2Char) {
             System.out.println("    " + s1Char + " is a part of a common subsequence");// same character will be printed many times because LCA function will be called many times with the same parameters. So, use Dynamic Programming.
-            return 1 + LCA_Brute_Force(S1, S2, s1Start, s1End - 1, s2Start, s2End - 1);
+            return 1 + LCS_Brute_Force(S1, S2, s1Start, s1End - 1, s2Start, s2End - 1);
         }
 
         // otherwise, get the max of (reducing first string's end index by 1, reducing second string's end index by 1)
-        return Math.max(LCA_Brute_Force(S1, S2, s1Start, s1End - 1, s2Start, s2End),
-                LCA_Brute_Force(S1, S2, s1Start, s1End, s2Start, s2End - 1));
+        return Math.max(LCS_Brute_Force(S1, S2, s1Start, s1End - 1, s2Start, s2End),
+                LCS_Brute_Force(S1, S2, s1Start, s1End, s2Start, s2End - 1));
     }
 
     private static int count = 0;
 
-    private static int LCA_Top_Down_Approach(char[] S1, char[] S2, int s1Start, int s1End, int s2Start, int s2End, Map<String, Integer> memo) {
+    private static int LCS_Top_Down_Approach(char[] S1, char[] S2, int s1Start, int s1End, int s2Start, int s2End, Map<String, Integer> memo) {
         count++;
 
         String key = s1End + ":" + s2End;
@@ -148,16 +148,16 @@ public class _4LongestCommonSubSequence {
         // if both chars matches, then reduce both strings' end index by 1
         if (s1Char == s2Char) {
             System.out.println("    " + s1Char + " is a part of a common subsequence");// same character will be printed many times because LCA function will be called many times with the same parameters. So, use Dynamic Programming.
-            int resultFromRemaining = LCA_Top_Down_Approach(S1, S2, s1Start, s1End - 1, s2Start, s2End - 1, memo);
+            int resultFromRemaining = LCS_Top_Down_Approach(S1, S2, s1Start, s1End - 1, s2Start, s2End - 1, memo);
             memo.put((s1End - 1) + ":" + (s2End - 1), resultFromRemaining);
             return 1 + resultFromRemaining;
         }
 
         // otherwise, get the max of (reducing first string's end index by 1, reducing second string's end index by 1)
-        int resultFromRemainingS1 = LCA_Top_Down_Approach(S1, S2, s1Start, s1End - 1, s2Start, s2End, memo);
+        int resultFromRemainingS1 = LCS_Top_Down_Approach(S1, S2, s1Start, s1End - 1, s2Start, s2End, memo);
         memo.put((s1End - 1) + ":" + s2End, resultFromRemainingS1);
 
-        int resultFromRemainingS2 = LCA_Top_Down_Approach(S1, S2, s1Start, s1End, s2Start, s2End - 1, memo);
+        int resultFromRemainingS2 = LCS_Top_Down_Approach(S1, S2, s1Start, s1End, s2Start, s2End - 1, memo);
         memo.put(s1End + ":" + (s2End - 1), resultFromRemainingS2);
 
         return Math.max(resultFromRemainingS1, resultFromRemainingS2);
