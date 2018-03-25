f9cfd2601a503ff5bb55e0a54ee581b5eef8916c
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-03-25 15:23:29 -0700

    more

--- src/main/java/algorithms/_1array/geeksforgeeks/dynamic_programming/string_subsequence_problems/_4LongestRepeatingNonOverlappingSubString.java
+++ src/main/java/algorithms/_1array/geeksforgeeks/dynamic_programming/string_subsequence_problems/_4LongestRepeatingNonOverlappingSubString.java
@@ -1,5 +1,8 @@
 package algorithms._1array.geeksforgeeks.dynamic_programming.string_subsequence_problems;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 Longest repeating and non-overlapping substring:
 https://www.geeksforgeeks.org/longest-repeating-and-non-overlapping-substring/
@@ -19,29 +22,46 @@ public class _4LongestRepeatingNonOverlappingSubString {
         {
             int substringLength = Brute_Force(s1.toCharArray(), s1.toCharArray(), 0, s1.length() - 1, 0, s1.length() - 1, false);
             System.out.println(substringLength);
+//            System.out.println(count);
         }
-
+        System.out.println();
+        System.out.println();
+        System.out.println();
         {
-            String subString = Bottom_Up_From_Site(s1.toCharArray(), s1.length());
+            int substringLength = Top_Down_Dynamic_Programming(s1.toCharArray(), s1.toCharArray(), 0, s1.length() - 1, 0, s1.length() - 1, false, new HashMap<>());
+            System.out.println(substringLength);
+//            System.out.println(cnt);
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        {
+            String subString = Bottom_Up_Dynamic_Programming_From_Site(s1.toCharArray(), s1.length());
             System.out.println(subString);
         }
 
     }
-
+    private static int count = 0;
+    // O(n^3).
+    // It can be converted close to O(n^2) using Dynamic approach
     private static int Brute_Force(char[] S1, char[] S2, int s1Start, int s1End, int s2Start, int s2End, boolean breakIfNotSame) {
+        count++;
+
         if (s1End < s1Start || s2End < s2Start) return 0;
 
         int finalMax = 0;
 
         for (int i = s1Start; i <= s1End; i++) {
+            int remaining = 0;
             for (int j = s2Start; j <= s2End; j++) {
                 if ((S1[i] == S2[j]) &&
+                        // important to make sure that comparison in between different indices.
                         (j > i) &&
                         // condition for non-overlapping
-                        ((j - i) > Brute_Force(S1, S2, i + 1, s1End, j + 1, s2End, true))) {
-                    System.out.println(S1[i] + " duplicate found");
+                        ((j - i) > (remaining = Brute_Force(S1, S2, i + 1, s1End, j + 1, s2End, true)))) {
+                    //System.out.println(S1[i] + " duplicate found");
 
-                    int max = 1 + Brute_Force(S1, S2, i + 1, s1End, j + 1, s2End, true);
+                    int max = 1 + remaining;
 
                     if (finalMax < max) {
                         finalMax = max;
@@ -58,7 +78,59 @@ public class _4LongestRepeatingNonOverlappingSubString {
         return finalMax;
     }
 
-    private static String Bottom_Up_From_Site(char[] str, int n) {
+    private static int cnt = 0;
+    private static int Top_Down_Dynamic_Programming(char[] S1, char[] S2, int s1Start, int s1End, int s2Start, int s2End, boolean breakIfNotSame, Map<String, Integer> memo) {
+        String key = s1Start + ":" + s2Start;
+
+        if (memo.get(key) != null) {
+            return memo.get(key);
+        }
+
+        cnt++;
+
+        if (s1End < s1Start || s2End < s2Start) return 0;
+
+        int finalMax = 0;
+        int startIndexOfSubString = 0;
+        int endIndexOfSubString = 0;
+
+        for (int i = s1Start; i <= s1End; i++) {
+            for (int j = s2Start; j <= s2End; j++) {
+                if ((S1[i] == S2[j]) && (j > i)) {
+                    int maxFromRemaining = Top_Down_Dynamic_Programming(S1, S2, i + 1, s1End, j + 1, s2End, true, memo);
+
+                    memo.put(key, maxFromRemaining);
+
+                    if ((j - i) > maxFromRemaining) {
+
+                        int max = 1 + maxFromRemaining;
+
+                        if (finalMax < max) {
+                            finalMax = max;
+                            startIndexOfSubString = i;
+                            endIndexOfSubString = startIndexOfSubString+(finalMax-1);
+                        }
+                    } else {
+
+                        if (breakIfNotSame) {
+                            break;
+                        }
+                    }
+                } else {
+
+                    if (breakIfNotSame) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println("startIndexOfSubString: "+startIndexOfSubString);
+        System.out.println("endIndexOfSubString: "+endIndexOfSubString);
+        return finalMax;
+    }
+
+    private static String Bottom_Up_Dynamic_Programming_From_Site(char[] str, int n) {
         String res = ""; // To store result
         int res_length = 0; // To store length of result
         int LCSRe[][] = new int[n + 1][n + 1];
