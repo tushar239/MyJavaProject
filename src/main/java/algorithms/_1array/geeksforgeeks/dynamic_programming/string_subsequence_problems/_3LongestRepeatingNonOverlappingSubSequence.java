fbafdaa05e549aef0d263373fae64dad205ada60
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-03-25 15:04:55 -0700

    more

---
+++ src/main/java/algorithms/_1array/geeksforgeeks/dynamic_programming/string_subsequence_problems/_3LongestRepeatingNonOverlappingSubSequence.java
@@ -0,0 +1,42 @@
+package algorithms._1array.geeksforgeeks.dynamic_programming.string_subsequence_problems;
+
+/*
+    Longest repeating and non-overlapping subsequence
+
+*/
+public class _3LongestRepeatingNonOverlappingSubSequence {
+
+    public static void main(String[] args) {
+        //String s1 = "gekshforgeksih";// 4- geks
+        //String s1 = "gekshforgekih";//3 - gek
+        //String s1 = "geeksforgeeks";//5 - geeks
+        String s1 = "geegkeek";//3 - gee
+//        String s1 = "gbekgsibek";//4 - gbek
+//        String s1 = "geekgsieek";//3 - geek
+//        String s1 = "geekgbeek";//4 - geek
+        int substringLength = LCS_Brute_Force(s1.toCharArray(), s1.toCharArray(), 0, s1.length() - 1, 0, s1.length() - 1);
+        System.out.println(substringLength);
+
+    }
+
+    private static int LCS_Brute_Force(char[] S1, char[] S2, int s1Start, int s1End, int s2Start, int s2End) {
+        if (s1End < s1Start || s2End < s2Start) return 0;
+
+        // reducing the problem by one
+        char s1Char = S1[s1End];
+        char s2Char = S2[s2End];
+
+        // if both chars matches, then reduce both strings' end index by 1
+        if ((s1Char == s2Char) &&
+                (s2End > s1End) &&
+                (s2End - s1End) > LCS_Brute_Force(S1, S2, s1Start, s1End - 1, s2Start, s2End - 1)) {
+
+            return 1 + LCS_Brute_Force(S1, S2, s1Start, s1End - 1, s2Start, s2End - 1);
+        }
+
+        // otherwise, get the max of (reducing first string's end index by 1, reducing second string's end index by 1)
+        return Math.max(LCS_Brute_Force(S1, S2, s1Start, s1End - 1, s2Start, s2End),
+                LCS_Brute_Force(S1, S2, s1Start, s1End, s2Start, s2End - 1));
+    }
+
+}
---
+++ src/main/java/algorithms/_1array/geeksforgeeks/dynamic_programming/string_subsequence_problems/_4LongestRepeatingNonOverlappingSubString.java
@@ -0,0 +1,95 @@
+package algorithms._1array.geeksforgeeks.dynamic_programming.string_subsequence_problems;
+
+/*
+Longest repeating and non-overlapping substring:
+https://www.geeksforgeeks.org/longest-repeating-and-non-overlapping-substring/
+
+*/
+public class _4LongestRepeatingNonOverlappingSubString {
+
+    public static void main(String[] args) {
+        //String s1 = "geekgeek";//4 - geek
+        //String s1 = "gekshforgeksih";// 4- geks
+        //String s1 = "gekshforgekih";//3 - gek
+        //String s1 = "geeksforgeeks";//5 - geeks
+        //String s1 = "gbekgsibek";//3 - bek
+        //String s1 = "geekgsieek";//3 - eek
+        String s1 = "geekgbeek";//3 - eek
+
+        {
+            int substringLength = Brute_Force(s1.toCharArray(), s1.toCharArray(), 0, s1.length() - 1, 0, s1.length() - 1, false);
+            System.out.println(substringLength);
+        }
+
+        {
+            String subString = Bottom_Up_From_Site(s1.toCharArray(), s1.length());
+            System.out.println(subString);
+        }
+
+    }
+
+    private static int Brute_Force(char[] S1, char[] S2, int s1Start, int s1End, int s2Start, int s2End, boolean breakIfNotSame) {
+        if (s1End < s1Start || s2End < s2Start) return 0;
+
+        int finalMax = 0;
+
+        for (int i = s1Start; i <= s1End; i++) {
+            for (int j = s2Start; j <= s2End; j++) {
+                if ((S1[i] == S2[j]) &&
+                        (j > i) &&
+                        ((j - i) > Brute_Force(S1, S2, i + 1, s1End, j + 1, s2End, true))) {
+                    System.out.println(S1[i] + " duplicate found");
+
+                    int max = 1 + Brute_Force(S1, S2, i + 1, s1End, j + 1, s2End, true);
+
+                    if (finalMax < max) {
+                        finalMax = max;
+                    }
+                } else {
+
+                    if (breakIfNotSame) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return finalMax;
+    }
+
+    private static String Bottom_Up_From_Site(char[] str, int n) {
+        String res = ""; // To store result
+        int res_length = 0; // To store length of result
+        int LCSRe[][] = new int[n + 1][n + 1];
+
+        // building table in bottom-up manner
+        int i, index = 0;
+        for (i = 1; i <= n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                // (j-i) > LCSRe[i-1][j-1] to remove overlapping
+                if (str[i - 1] == str[j - 1] &&
+                        LCSRe[i - 1][j - 1] < (j - i)) {
+                    LCSRe[i][j] = LCSRe[i - 1][j - 1] + 1;
+
+                    // updating maximum length of the
+                    // substring and updating the finishing
+                    // index of the suffix
+                    if (LCSRe[i][j] > res_length) {
+                        res_length = LCSRe[i][j];
+                        index = Math.max(i, index);
+                    }
+                } else
+                    LCSRe[i][j] = 0;
+            }
+        }
+
+        // If we have non-empty result, then insert all
+        // characters from first character to last
+        // character of string
+        if (res_length > 0)
+            for (i = index - res_length + 1; i <= index; i++)
+                res = res + (str[i - 1]);
+
+        return res;
+    }
+}
