f51c796395862e5bc709be0db244823fc624d08e
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-03-28 23:12:44 -0700

    more

--- src/main/java/algorithms/_1array/geeksforgeeks/dynamic_programming/_2subsequence_substring_in_string_problems/_2palindrom_subsequence_substring/_1LPS_Longest_Palindromic_Subsequence_Substring_Algorithms/_2substring/_2LongestPalindromicSubstring_Using_Manachers_Algorithm.java
+++ src/main/java/algorithms/_1array/geeksforgeeks/dynamic_programming/_2subsequence_substring_in_string_problems/_2palindrom_subsequence_substring/_1LPS_Longest_Palindromic_Subsequence_Substring_Algorithms/_2substring/_2LongestPalindromicSubstring_Using_Manachers_Algorithm.java
@@ -1,7 +1,15 @@
 package algorithms._1array.geeksforgeeks.dynamic_programming._2subsequence_substring_in_string_problems._2palindrom_subsequence_substring._1LPS_Longest_Palindromic_Subsequence_Substring_Algorithms._2substring;
 
 /*
- You use Dynamic Programming to find 'Longest Palindromic Substring Length In A String' in O(n^2)
+ Manacher's Algorithm to find 'Longest Palindromic Substring'.
+
+ https://www.geeksforgeeks.org/manachers-algorithm-linear-time-longest-palindromic-substring-part-1/
+ https://www.geeksforgeeks.org/manachers-algorithm-linear-time-longest-palindromic-substring-part-2/
+ https://www.geeksforgeeks.org/manachers-algorithm-linear-time-longest-palindromic-substring-part-3-2/
+ https://www.geeksforgeeks.org/manachers-algorithm-linear-time-longest-palindromic-substring-part-4/
+
+
+ You can use Dynamic Programming to find 'Longest Palindromic Substring Length In A String' in O(n^2)
  LongestPalindromicSubstring_Using_Dynamic_Programming_On2_TimeComplexity.java
 
  You can use Manacher's Algorithm to make it possible in O(n).
