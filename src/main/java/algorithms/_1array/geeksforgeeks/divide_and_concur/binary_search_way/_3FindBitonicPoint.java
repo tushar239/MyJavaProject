cce66adcf7feac298fe3b46748b8e0724a634f39
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-04-07 13:53:27 -0700

    more

--- src/main/java/algorithms/_1array/geeksforgeeks/divide_and_concur/binary_search_way/_3FindBitonicPoint.java
+++ src/main/java/algorithms/_1array/geeksforgeeks/divide_and_concur/binary_search_way/_3FindBitonicPoint.java
@@ -21,23 +21,40 @@ public class _3FindBitonicPoint {
     public static void main(String[] args) {
         {
             int arr[] = {6, 7, 8, 11, 9, 5, 2, 1};
+
             int index = binarySearch(arr, 0, arr.length - 1);
             System.out.println("index: " + index);//3
+
+            index = binarySearch_another(arr, 1, arr.length - 2);
+            System.out.println("index: " + index);//3
+
         }
         {
             int arr[] = {6, 11, 10, 9, 5, 2, 1};
+
             int index = binarySearch(arr, 0, arr.length - 1);
+            System.out.println("index: " + index);//1
+
+            index = binarySearch_another(arr, 1, arr.length - 2);
             System.out.println("index: " + index);//1
         }
         {
             int arr[] = {6, 7, 8, 11, 12, 13, 14, 15};
             int index = binarySearch(arr, 0, arr.length - 1);
             System.out.println("index: " + index);//-1
+
+            index = binarySearch_another(arr, 1, arr.length - 2);
+            System.out.println("index: " + index);//-1
+
         }
         {
             int arr[] = {6, 5, 4, 3, 2, 1};
             int index = binarySearch(arr, 0, arr.length - 1);
             System.out.println("index: " + index);//-1
+
+            index = binarySearch_another(arr, 1, arr.length - 2);
+            System.out.println("index: " + index);//-1
+
         }
 
     }
@@ -58,6 +75,26 @@ public class _3FindBitonicPoint {
             return binarySearch(arr, start, mid - 1);
         }
         return binarySearch(arr, mid + 1, end);
+
+    }
+
+    // instead of start=0, send start=1 and instead of sending end=A.length-1, send A.length-2
+    private static int binarySearch_another(int arr[], int start, int end) {
+        if (start > end) return -1;
+
+        int mid = (start + end) / 2;
+
+        //if (mid == start || mid == end) return -1;// important exit condition
+
+        // exit condition to check if arr[mid] is bitonic point or not
+        if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) {
+            return mid;
+        }
+
+        if (arr[mid + 1] < arr[mid]) {
+            return binarySearch_another(arr, start, mid - 1);
+        }
+        return binarySearch_another(arr, mid + 1, end);
 
     }
 }
