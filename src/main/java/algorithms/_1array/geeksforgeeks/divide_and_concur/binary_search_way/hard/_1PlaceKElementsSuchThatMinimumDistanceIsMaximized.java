897bb1993cd73369d8d0c842083614f158ccc199
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-04-07 18:51:29 -0700

    more

--- src/main/java/algorithms/_1array/geeksforgeeks/divide_and_concur/binary_search_way/hard/_1PlaceKElementsSuchThatMinimumDistanceIsMaximized.java
+++ src/main/java/algorithms/_1array/geeksforgeeks/divide_and_concur/binary_search_way/hard/_1PlaceKElementsSuchThatMinimumDistanceIsMaximized.java
@@ -10,69 +10,86 @@ import java.util.Arrays;
     This problem is harder to understand.
     It says that place K objects on K array elements in such a way that distance between any two objects can be maximized.
 
+    1. sort an array, so that it is easier to place the objects and find the distance between them.
+    2. decide distance between to objects as mid element (  left=A[start], right=A[end], distance=(left+right)/2  )
+    3. iterate entire array.
+        place 1st object on 1st element always because that's the smallest possible element and one object should always be placed on it.
+        find second element in remaining array whose distance from 1st object is >= mid
+        then third second element, whose distance from 2nd object is >= mid
+        and so on till you place K objects.
+
+        If you can't place all K objects with distance from each other >= mid, then you need to reduce the distance (right=mid) and start from step 2 again.
+        If you can place all K objects with distance from each other >= mid, then you need to increase the distance (left=mid+1) and start from step 2 again.
+
+    example:
+
+    points on a line : [1   2   4   8   9], place 3 objects on a line in such a way that distance between adjacent objects can be maximized.
+
+        distance = mid = (1+9)/2 = 5
+        first object can be placed on 1, second object on 8, there is no place for 9
+        so, reduce the distance (right=5)
+
+        distance = mid = (1+5)/2 = 3
+        first object can be placed on 1, second object on 4, third object can be placed on 8
+        Now, try with higher distance (left=3+1=4)
+
+        distance = mid = (4+5)/2 = 4
+        first object can be placed on 1, second object on 8, there is no place for 9
+
+        So, max distance between any two adjacent objects out of K objects can be 3
+
+
 */
 public class _1PlaceKElementsSuchThatMinimumDistanceIsMaximized {
 
     public static void main(String[] args) {
         int arr[] = {1, 2, 8, 4, 9};
         int n = arr.length;
-        int k = 3;
-        System.out.print(largestMinDist(arr, n, k));
+        int KObjects = 3;
+        System.out.print(largestMinDist(arr, 0,n-1, KObjects));
     }
 
-    // Returns largest minimum distance for
-    // k elements in arr[0..n-1]. If elements
-    // can't be placed, returns -1.
-    static int largestMinDist(int arr[], int n,
-                              int k) {
+    // Returns largest minimum distance for K Objects elements in arr[0..end-1].
+    // If elements can't be placed, returns -1.
+    static int largestMinDist(int arr[], int start, int end, int KObjects) {
         // Sort the positions
         Arrays.sort(arr);
 
         // Initialize result.
-        int res = -1;
+        int maxPossibleDistance = -1;
 
         // Consider the maximum possible distance
-        int left = arr[0], right = arr[n - 1];
+        int left = arr[start], right = arr[end];
 
-        // Do binary search for largest
-        // minimum distance
+        // Do binary search for largest minimum distance
         while (left < right) {
-            int mid = (left + right) / 2;
+            int mid = (left + right) / 2;// min distance between two objects
 
-            // If it is possible to place k
-            // elements with minimum distance mid,
-            // search for higher distance.
-            if (isFeasible(mid, arr, n, k)) {
-                // Change value of variable max to
-                // mid if all elements can be
-                // successfully placed
-                res = Math.max(res, mid);
+            // If it is possible to place K Objects elements with minimum distance mid, search for higher distance.
+            if (isFeasible(mid, arr, start, end, KObjects)) {
+                // Change value of variable max to mid if all elements can be successfully placed
+                maxPossibleDistance = Math.max(maxPossibleDistance, mid);
                 left = mid + 1;
             }
 
-            // If not possible to place k elements,
-            // search for lower distance
+            // If not possible to place K Objects elements, search for lower distance
             else
                 right = mid;
         }
 
-        return res;
+        return maxPossibleDistance;
     }
 
-    // Returns true if it is possible to
-    // arrange k elements of arr[0..n-1]
-    // with minimum distance given as mid.
-    static boolean isFeasible(int mid, int arr[],
-                              int n, int k) {
+    // Returns true if it is possible to arrange K Objects elements of arr[0..end-1] with minimum distance given as mid.
+    static boolean isFeasible(int mid, int arr[], int start, int end, int KObjects) {
         // Place first element at arr[0] position
-        int pos = arr[0];
+        int pos = arr[start];
 
         // Initialize count of elements placed.
         int elements = 1;
 
-        // Try placing k elements with minimum
-        // distance mid.
-        for (int i = 1; i < n; i++) {
+        // Try placing KObjects elements with minimum distance mid.
+        for (int i = start+1; i <= end; i++) {
             if (arr[i] - pos >= mid) {
                 // Place next element if its
                 // distance from the previously
@@ -83,7 +100,7 @@ public class _1PlaceKElementsSuchThatMinimumDistanceIsMaximized {
 
                 // Return if all elements are
                 // placed successfully
-                if (elements == k)
+                if (elements == KObjects)
                     return true;
             }
         }
