a14f9e19a1cd4f2f81efda2b7be17080424a0a8a
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-05-10 14:43:18 -0700

    more

--- src/main/java/algorithms/utils/ArrayUtils.java
+++ src/main/java/algorithms/utils/ArrayUtils.java
@@ -44,6 +44,12 @@ public class ArrayUtils {
         arr[j] = element;
     }
 
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
     public static void printArray(int[] arr, Supplier<String> outputSupplier) {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(String.format(outputSupplier.get(), i, arr[i]));
