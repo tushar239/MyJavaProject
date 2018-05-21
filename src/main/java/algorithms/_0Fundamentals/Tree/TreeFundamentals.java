e21d16e7815699593a6b5fd47a7e93770847517a
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-05-20 17:04:45 -0700

    more

--- src/main/java/algorithms/_0Fundamentals/Tree/TreeFundamentals.java
+++ src/main/java/algorithms/_0Fundamentals/Tree/TreeFundamentals.java
@@ -28,7 +28,9 @@ Tree:
     ------------------------
 
     Any Binary Tree (not just Binary Search Tree) can be traversed 4 ways
+
     See 'Binary Tree Traversal Types.doc'
+    See 'PreOrder,InOrder,PostOrder Traversal tricks.mp4'
 
     https://www.youtube.com/watch?v=9RHO6jU--GU
 
@@ -177,6 +179,53 @@ Tree:
     \
     6
     have the same inorder traversal as 4,30,10,6 but the second one isn't subtree
+
+
+    Searching a node involves traversing a tree. There are different types of traversals.
+
+        - Pre Order
+        - In Order
+        - Post Order
+        - Level Order
+
+        See BST.java
+
+    When you traverse a
+
+    -   Normal Binary Tree,
+        To search an element, you may need to go both left and right, if element is not same as root node.
+
+        Here, is a sample code to find an element recursively in a Binary Tree.
+
+        if(element == root.data) {
+            return true;
+        }
+
+        boolean foundInLeftSubTree = find(root.left, element);
+
+        boolean foundInRightSubTree = false;
+
+        if (!foundInLeftSubTree) {
+            foundInRightSubTree = find(root.left, element);
+        }
+
+        e.g. ReverseBinaryTreePath.java
+
+    -   Binary Search Tree(BST),
+
+        You need to apply 'Binary Search' concept.
+        If node that you are searching is less than root node, then traverse left subtree, otherwise traverse left search tree.
+
+        Here, is a sample code to find an element recursively in a Binary Search Tree (BST).
+
+        if(element == root.data) {
+            return true;
+        }
+        if(element < root.data) {
+            return find(root.left, element);
+        }
+        return find(root.right, element);
+
 */
 
 /*
@@ -429,50 +478,6 @@ A node in a trie could have anywhere from 0 to ALPHABET_SIZE + 1 children.
 
 - searching
 
-    Searching involves traversing a tree. There are different types of traversals.
-
-        - Pre Order
-        - In Order
-        - Post Order
-        - Level Order
-
-        See BST.java, Tree.java
-
-    When you traverse a
-
-    -   Normal Binary Tree,
-        To search an element, you may need to go both left and right, if element is not same as root node.
-
-        Here, is a sample code to find an element recursively in a Binary Tree.
-
-        if(element == root.data) {
-            return true;
-        }
-
-        boolean foundInLeftSubTree = find(root.left, element);
-
-        boolean foundInRightSubTree = false;
-
-        if (!foundInLeftSubTree) {
-            foundInRightSubTree = find(root.left, element);
-        }
-
-        e.g. ReverseBinaryTreePath.java
-
-    -   Binary Search Tree(BST),
-
-        You need to apply 'Binary Search' concept.
-        If node that you are searching is less than root node, then traverse left subtree, otherwise traverse left search tree.
-
-        Here, is a sample code to find an element recursively in a Binary Search Tree (BST).
-
-        if(element == root.data) {
-            return true;
-        }
-        if(element < root.data) {
-            return find(root.left, element);
-        }
-        return find(root.right, element);
 
 */
 
