1a64711cc4e335be18d04a04a6e5af25b0a943c2
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-07-10 21:11:30 -0700

    more

--- src/main/java/algorithms/_1array_stack_queue/geeksforgeeks/linked_list/medium/_2ImplementHashingWithSimpleChainingAndSinglyLinkedList.java
+++ src/main/java/algorithms/_1array_stack_queue/geeksforgeeks/linked_list/medium/_2ImplementHashingWithSimpleChainingAndSinglyLinkedList.java
@@ -3,43 +3,54 @@ package algorithms._1array_stack_queue.geeksforgeeks.linked_list.medium;
 import java.util.LinkedList;
 
 /*
-    Hashing with Collision avoidance technique Separate Chaining
+    Hashtable chaining with Separate Chaining and Singly LinkedList
 
     https://www.geeksforgeeks.org/c-program-hashing-chaining/
+
+
+    Hashtable chaining with Separate Chaining and Doubly LinkedList
+    https://www.geeksforgeeks.org/hashtables-chaining-with-doubly-linked-lists/
+
+    I personally don't see any benefit of using doubly linkedlist because to search/remove an element you always start from the beginning of the list, you never do that from the middle of the list.
+    If there is a need to remove/search an element from the middle of the list, then doubly linkedlist makes sense.
+
+
+    Question for interviewer:
+    Can I use java's inbuilt linkedlist or should I created my own?
+
 */
 public class _2ImplementHashingWithSimpleChainingAndSinglyLinkedList {
-    private static final int HASHTABLE_SIZE = 5;
+    private static final int HASH_TABLE_SIZE = 5;
     // array of linked lists
 
     public static void main(String[] args) {
-        chaining();
-    }
-
-    private static void chaining() {
-        LinkedList<String>[] hashTable = new LinkedList[HASHTABLE_SIZE];
-
-        for (int i = 0; i < HASHTABLE_SIZE; i++) {
+        LinkedList<String>[] hashTable = new LinkedList[HASH_TABLE_SIZE];
+        for (int i = 0; i < HASH_TABLE_SIZE; i++) {
             hashTable[i] = new LinkedList<>();
         }
 
+        chaining(hashTable);
+    }
+
+    private static void chaining(LinkedList<String>[] hashTable) {
         String s1 = "abc";
         String s2 = "def";
         String s3 = "xyz";
-        String s4 = "tushar chokshi";
+        String s4 = "TUSH CHOK";
         String s5 = "jkl";
         String s6 = "imd";
 
-        hashTable[getArrayIndex(s1, HASHTABLE_SIZE)].addFirst(s1);
-        hashTable[getArrayIndex(s2, HASHTABLE_SIZE)].addFirst(s2);
-        hashTable[getArrayIndex(s3, HASHTABLE_SIZE)].addFirst(s3);
-        hashTable[getArrayIndex(s4, HASHTABLE_SIZE)].addFirst(s4);
-        hashTable[getArrayIndex(s5, HASHTABLE_SIZE)].addFirst(s5);
-        hashTable[getArrayIndex(s6, HASHTABLE_SIZE)].addFirst(s6);
+        hashTable[getArrayIndex(s1, HASH_TABLE_SIZE)].addFirst(s1);
+        hashTable[getArrayIndex(s2, HASH_TABLE_SIZE)].addFirst(s2);
+        hashTable[getArrayIndex(s3, HASH_TABLE_SIZE)].addFirst(s3);
+        hashTable[getArrayIndex(s4, HASH_TABLE_SIZE)].addFirst(s4);
+        hashTable[getArrayIndex(s5, HASH_TABLE_SIZE)].addFirst(s5);
+        hashTable[getArrayIndex(s6, HASH_TABLE_SIZE)].addFirst(s6);
 
         System.out.println("Created hashed hashTable.....");
         System.out.println();
 
-        for (int i = 0; i < HASHTABLE_SIZE; i++) {
+        for (int i = 0; i < HASH_TABLE_SIZE; i++) {
             System.out.print(i+"th element: ");
             for (String s : hashTable[i]) {
                 System.out.print(s+",");
@@ -51,7 +62,7 @@ public class _2ImplementHashingWithSimpleChainingAndSinglyLinkedList {
         System.out.println("Trying to find a string in hashed hashTable.....");
         System.out.println();
 
-        String foundString = search(s1, hashTable, HASHTABLE_SIZE);
+        String foundString = search(s1, hashTable, HASH_TABLE_SIZE);
         System.out.println("found string:" + foundString);
     }
 
