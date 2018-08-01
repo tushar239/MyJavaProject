043ce82fbb60e82e2ba364f89179ce25166a0f7c
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-08-01 14:03:31 -0700

    more

--- src/main/java/algorithms/_3binary_tree/geeksforgeeks/BFS/_3pre_order_traversal/_1_2PrintAllRootToLeafPathsOfBinaryTreeWithGivenSum.java
+++ src/main/java/algorithms/_3binary_tree/geeksforgeeks/BFS/_3pre_order_traversal/_1_2PrintAllRootToLeafPathsOfBinaryTreeWithGivenSum.java
@@ -2,6 +2,8 @@ package algorithms._3binary_tree.geeksforgeeks.BFS._3pre_order_traversal;
 
 /*
     Print Root to Leaf Path with Given sum(Print all K-Sum paths) in a given Binary Tree
+    OR
+    Print all k-sum paths in a binary tree
 
     This is just an extension of 'PrintAllRootToLeafPathsOfBinaryTree.java'.
     In 'PrintAllRootToLeafPathsOfBinaryTree.java', when you are printing a list, before printing it just sum its elements and check whether sum is same as expected.
