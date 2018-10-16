3c6f218c39ec615d9620934451484d4507e64df2
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-10-15 19:33:30 -0700

    more

--- src/main/java/algorithms/_1array_stack_queue/geeksforgeeks/pointers_sorting_stack_queue_string/hard/_6FindMajorityElementInArray.java
+++ src/main/java/algorithms/_1array_stack_queue/geeksforgeeks/pointers_sorting_stack_queue_string/hard/_6FindMajorityElementInArray.java
@@ -10,6 +10,50 @@ result=4
 
 Definition of Majority element:
 if number of occurrences of any element > size of array/2, then that element is in majority.
+
+Solution 1:
+Create a map of array element and number of time it appears.
+Find an element that appears max times.
+Then just see whether that max time > arraySize/2
+
+This solution uses extra space (a map).
+
+Solution 2:
+
+4,      7,      4,      4,      7,      4,      4,      9,      4,      3
+ele=4
+cnt=1
+        ele=7
+        cnt=1
+
+                ele=4
+                cnt=1
+
+                        ele=4
+                        cnt=2
+
+                                ele=4
+                                cnt=1
+
+                                        ele=4
+                                        cnt=2
+
+                                                ele=4
+                                                cnt=3
+                                                        ele=4
+                                                        cnt=3
+                                                                ele=4
+                                                                cnt=3
+
+                                                                        ele=4
+                                                                        cnt=1
+
+
+here ele (ele of interest) is 4
+if cnt==0, then there is no element that can be considered a majority element.
+
+now loop entire array to find out how many times 4 appears. (ans = 6)
+6 > A.length/2. So 4 is a majority element
 */
 public class _6FindMajorityElementInArray {
 
@@ -58,6 +102,33 @@ public class _6FindMajorityElementInArray {
     }
 
     private static int findMajorityElement(int[] A) throws Exception {
+        int majorityElement = Integer.MIN_VALUE;
+        int count = 0;
+
+        for (int num : A) {
+            if (count == 0) {
+                majorityElement = num;
+                count++;
+            } else {
+                if (num == majorityElement) {
+                    count++;
+                } else {
+                    count--;
+                    if (count == 0) {
+                        majorityElement = num;
+                        count++;
+                    }
+                }
+            }
+        }
+
+        if (count > 0) {
+            return majorityElement;
+        }
+        throw new Exception("there can be more than one element that appears same number of times.");
+    }
+
+    /*private static int findMajorityElement(int[] A) throws Exception {
         int majorityElement = 0;
         int count = 0;
 
@@ -82,7 +153,7 @@ public class _6FindMajorityElementInArray {
             return majorityElement;
         }
         throw new Exception("there can be more than one element that appears same number of times.");
-    }
+    }*/
 
     private static boolean findWhetherMajorityElementAppearsMostOfTheTime(int A[], int majorityElement) {
         int count = 0;
