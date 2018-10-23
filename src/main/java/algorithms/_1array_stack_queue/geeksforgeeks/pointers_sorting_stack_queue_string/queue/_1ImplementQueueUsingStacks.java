3d075f53f3edf5119fed6f0cdc85aeb6e99ad495
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-10-22 21:14:01 -0700

    more

--- src/main/java/algorithms/_1array_stack_queue/geeksforgeeks/pointers_sorting_stack_queue_string/queue/_1ImplementQueueUsingStacks.java
+++ src/main/java/algorithms/_1array_stack_queue/geeksforgeeks/pointers_sorting_stack_queue_string/queue/_1ImplementQueueUsingStacks.java
@@ -1,5 +1,7 @@
 package algorithms._1array_stack_queue.geeksforgeeks.pointers_sorting_stack_queue_string.queue;
 
+import java.util.Stack;
+
 /*
     Implement Queue using Stacks
 
@@ -13,6 +15,104 @@ package algorithms._1array_stack_queue.geeksforgeeks.pointers_sorting_stack_queu
     When new element needs to be added, push it to S2 (empty stack) and then pop all elements from S1 and push them to S2.
     and then exchange S1 with S2 and vice-versa.
 
+    - add(1)
+
+    1
+    S1  S2
+
+    - add(2)
+
+    copy(S1,S2)
+    S2.add(2)
+    copy(S2,S1)
+
+                2       1
+         1      1       2
+    S1  S2      S2      S1
+
+    - add(3)
+
+    copy(S2,S1)
+    S1.add(3)
+    copy(S1,S2)
+
+
+        3       1
+        2       2
+        1       3
+    S1  S2      S1
+
+
+    and so on...
+
+
+
 */
 public class _1ImplementQueueUsingStacks {
+
+    public static void main(String[] args) {
+        QueueUsingStacks queue = new QueueUsingStacks();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+
+
+        System.out.println(queue.poll());//1
+        System.out.println(queue.poll());//2
+        System.out.println(queue.poll());//3
+        System.out.println(queue.poll());//4
+        System.out.println(queue.poll());//5
+        System.out.println(queue.poll());//null
+
+    }
+
+    private static class QueueUsingStacks {
+        Stack<Integer> S1 = new Stack<>();
+        Stack<Integer> S2 = new Stack<>();
+
+        // takes O(2 * n^2), so O(n^2)
+        private void add(Integer ele) {
+            System.out.println("adding ele: " + ele);
+            // if both stacks are empty, add element to any stack
+            if (S1.isEmpty() && S2.isEmpty()) {
+                S1.add(ele);
+            }
+            // first copy elements from non-empty stack(e.g. S1) to empty stack(e.g. S2)
+            // add new element to S2
+            // reverse the process of copying (S2 to S1)
+            else if (!S1.isEmpty() && S2.isEmpty()) {
+                copy(S1, S2);// O(n)
+                S2.add(ele);
+                copy(S2, S1);// O(n)
+            } else {
+                copy(S2, S1);
+                S1.add(ele);
+                copy(S1, S2);
+            }
+        }
+
+        private void copy(Stack<Integer> from, Stack<Integer> to) {
+            //int count =0;
+            while (!from.isEmpty()) {
+                to.push(from.pop());
+                //count++;
+            }
+            //System.out.println("count:"+count);
+        }
+
+        private Integer poll() {
+            if (!S1.isEmpty()) return S1.pop();
+            else if (!S2.isEmpty()) return S2.pop();
+            return null;
+        }
+
+        public Integer peek() {
+            if (!S1.isEmpty()) return S1.peek();
+            else if (!S2.isEmpty()) return S2.peek();
+            return null;
+        }
+
+    }
 }
--- src/main/java/algorithms/_1array_stack_queue/geeksforgeeks/pointers_sorting_stack_queue_string/stack/medium/_3ImplementStackUsingQueues.java
+++ src/main/java/algorithms/_1array_stack_queue/geeksforgeeks/pointers_sorting_stack_queue_string/stack/medium/_3ImplementStackUsingQueues.java
@@ -13,6 +13,9 @@ import java.util.concurrent.LinkedBlockingQueue;
     You can use two queues (Q1, Q2) to make them work like a stack.
     When new element needs to be added, add it to Q2 (empty queue) and then dequeue all elements from Q1 and enqueue them to Q2
     and then exchange Q1 with Q2 and vice-versa.
+
+
+    See ImplementQueueUsingStacks.java first.
 */
 public class _3ImplementStackUsingQueues {
 
@@ -36,14 +39,26 @@ public class _3ImplementStackUsingQueues {
     }*/
 
     private static void implementStackUsingQueues() {
-        MyStackUsingQueues myStackUsingQueues = new MyStackUsingQueues();
-        myStackUsingQueues.push(1);
-        myStackUsingQueues.push(2);
-        myStackUsingQueues.push(3);
-        System.out.println(myStackUsingQueues.poll());
-        System.out.println(myStackUsingQueues.poll());
-        System.out.println(myStackUsingQueues.poll());
-        System.out.println(myStackUsingQueues.poll());
+        {
+            MyStackUsingQueues myStackUsingQueues = new MyStackUsingQueues();
+            myStackUsingQueues.push(1);
+            myStackUsingQueues.push(2);
+            myStackUsingQueues.push(3);
+            System.out.println(myStackUsingQueues.pop());//3
+            System.out.println(myStackUsingQueues.pop());//2
+            System.out.println(myStackUsingQueues.pop());//1
+            System.out.println(myStackUsingQueues.pop());//null
+        }
+        {
+            MyStackUsingQueues_Another stack = new MyStackUsingQueues_Another();
+            stack.push(1);
+            stack.push(2);
+            stack.push(3);
+            System.out.println(stack.pop());//3
+            System.out.println(stack.pop());//2
+            System.out.println(stack.pop());//1
+            System.out.println(stack.pop());//null
+        }
     }
 
     private static class MyStackUsingQueues {
@@ -52,25 +67,68 @@ public class _3ImplementStackUsingQueues {
 
         public void push(Integer num) {
             Q2.add(num);
-            while(!Q1.isEmpty()) {
+            while (!Q1.isEmpty()) {
                 Q2.add(Q1.remove());
             }
             // exchange Q1 and Q2
             Queue<Integer> temp = Q1;
-            Q1=Q2;
-            Q2=temp;
+            Q1 = Q2;
+            Q2 = temp;
         }
 
-        public Integer poll() {
-            if(!Q1.isEmpty()) {
+        public Integer pop() {
+            if (!Q1.isEmpty()) {
                 return Q1.remove();
             }
             return null;
         }
+
         public Integer peek() {
-            if(!Q1.isEmpty()) {
+            if (!Q1.isEmpty()) {
                 return Q1.peek();
             }
+            return null;
+        }
+    }
+
+    private static class MyStackUsingQueues_Another {
+        private Queue<Integer> Q1 = new LinkedBlockingQueue<>();
+        private Queue<Integer> Q2 = new LinkedBlockingQueue<>();
+
+        public void push(Integer ele) {
+            // if both queues are empty, add element to any stack
+            if (Q1.isEmpty() && Q2.isEmpty()) {
+                Q1.add(ele);
+            }
+            // first copy elements from non-empty queue(e.g. Q1) to empty queue (e.g. Q2)
+            // add new element to Q1
+            // reverse the process of copying (Q2 to Q1)
+            else if (!Q1.isEmpty() && Q2.isEmpty()) {
+                copy(Q1, Q2);
+                Q1.add(ele);
+                copy(Q2, Q1);
+            } else {
+                copy(Q2, Q1);
+                Q2.add(ele);
+                copy(Q1, Q2);
+            }
+        }
+
+        private void copy(Queue<Integer> from, Queue<Integer> to) {
+            while (!from.isEmpty()) {
+                to.add(from.poll());
+            }
+        }
+
+        public Integer pop() {
+            if (!Q1.isEmpty()) return Q1.poll();
+            else if (!Q2.isEmpty()) return Q2.poll();
+            return null;
+        }
+
+        public Integer peek() {
+            if (!Q1.isEmpty()) return Q1.peek();
+            else if (!Q2.isEmpty()) return Q2.peek();
             return null;
         }
     }
