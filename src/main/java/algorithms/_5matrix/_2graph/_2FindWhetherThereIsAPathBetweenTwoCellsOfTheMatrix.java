7c744faa3e872ab0780a1b362914723a362c4914
Tushar Chokshi <tushar.chokshi@cdk.com>
2018-11-21 19:41:37 -0800

    more

---
+++ src/main/java/algorithms/_5matrix/_2graph/_2FindWhetherThereIsAPathBetweenTwoCellsOfTheMatrix.java
@@ -0,0 +1,171 @@
+package algorithms._5matrix._2graph;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/*
+
+    Find whether there is path between two cells in matrix
+
+    https://www.geeksforgeeks.org/find-whether-path-two-cells-matrix/
+
+
+    Given N X N matrix filled with 1 , 0 , 2 , 3 . Find whether there is a path possible from source to destination, traversing through blank cells only. You can traverse up, down, right and left.
+
+    - A value of cell 1 means Source.
+    - A value of cell 2 means Destination.
+    - A value of cell 3 means Blank cell.
+    - A value of cell 0 means Blank Wall.
+
+    Examples:
+
+    Input : M[3][3] = {{ 0 , 3 , 2 },
+                       { 3 , 3 , 0 },
+                       { 1 , 3 , 0 }};
+    Output : Yes
+
+    Input : M[4][4] = {{ 0 , 3 , 1 , 0 },
+                       { 3 , 0 , 3 , 3 },
+                       { 2 , 3 , 0 , 3 },
+                       { 0 , 3 , 3 , 3 }};
+    Output : Yes
+
+
+    Solution:
+
+    Matrix is one type of graph.
+
+    BFS has three main algorithms
+    - Find Shortest Path between two vertices of a graph.
+    - Find whether there is a path between two vertices of a graph.
+    - Find the distance between two vertices of a graph.
+
+    Read BfsDfsFromGrokkingAlgorithmBook.java.
+*/
+public class _2FindWhetherThereIsAPathBetweenTwoCellsOfTheMatrix {
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {0, 3, 2},
+                {3, 3, 0},
+                {1, 3, 0}
+        };
+        CellWithParent startingCell = new CellWithParent(2,0, null);
+        CellWithParent destinationCell = new CellWithParent(0,2, null);
+
+        boolean pathFromStartToDestinationCellsFound = BFS(matrix, startingCell, destinationCell);
+        System.out.println(pathFromStartToDestinationCellsFound);
+    }
+
+    private static boolean BFS(int[][] matrix, CellWithParent startingCell, CellWithParent destinationCell) {
+        if (matrix == null || matrix.length == 0) return false;
+
+        Queue<CellWithParent> queue = new LinkedBlockingQueue<>();
+        queue.add(startingCell);
+
+        List<CellWithParent> visitedCells = new LinkedList<>();
+        visitedCells.add(startingCell);
+
+        BFS(matrix, startingCell, destinationCell, queue, visitedCells);
+
+        for (CellWithParent visitedCell : visitedCells) {
+            if(visitedCell.getX() == destinationCell.getX() && visitedCell.getY() == destinationCell.getY()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void BFS(int[][] matrix, CellWithParent startingCell, CellWithParent destinationCell, Queue<CellWithParent> queue, List<CellWithParent> visitedCells) {
+        while (!queue.isEmpty()) {
+
+            CellWithParent cell = queue.poll();
+
+            if (cell.equals(destinationCell))
+                return; // you could reach to destination cell. it is already put in visitedCells.
+
+            int x = cell.getX();
+            int y = cell.getY();
+
+            CellWithParent up = new CellWithParent(x - 1, y, cell);
+            CellWithParent down = new CellWithParent(x + 1, y, cell);
+            CellWithParent left = new CellWithParent(x, y - 1, cell);
+            CellWithParent right = new CellWithParent(x, y + 1, cell);
+
+            if (isEligibleToAddToQueue(up, matrix, visitedCells)) { queue.add(up); visitedCells.add(up); }
+            if (isEligibleToAddToQueue(down, matrix, visitedCells)) { queue.add(down); visitedCells.add(down); }
+            if (isEligibleToAddToQueue(left, matrix, visitedCells)) { queue.add(left); visitedCells.add(left); }
+            if (isEligibleToAddToQueue(right, matrix, visitedCells)) { queue.add(right); visitedCells.add(right); }
+
+        }
+    }
+
+    private static boolean isEligibleToAddToQueue(CellWithParent cell, int[][] matrix, List<CellWithParent> visitedCells) {
+        int x = cell.getX();
+        int y = cell.getY();
+
+        // is the cell already visited?
+        // IMPORTANT: if(visitedCell.contains(cell)) will not work because cell has parentCell in it and same cell can be reached from more than one parents.
+        boolean isVisited = false;
+        for (CellWithParent visitedCell : visitedCells) {
+            if (visitedCell.getX() == cell.getX() && visitedCell.getY() == cell.getY()) {
+                isVisited = true;
+            }
+        }
+
+        // if cell is not yet visited AND cell is inside the range of matrix AND it is a blank or destination cell, then that cell is eligible to be added to queue
+        if (!isVisited) {// typical BFS condition
+            if (x >= 0 && x <= matrix.length - 1) { // in range
+                if (y >= 0 && y <= matrix[x].length - 1) { // in range
+                    if (matrix[x][y] == 3 || matrix[x][y] == 2 ) { // is a blank cell or destination cell. IMPORTANT: don't just consider blank cell. Also consider destination cell also.
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static class CellWithParent {
+        private int x;
+        private int y;
+        private CellWithParent parentCell;
+
+        public CellWithParent(int x, int y, CellWithParent parentCell) {
+            this.x = x;
+            this.y = y;
+            this.parentCell = parentCell;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public CellWithParent getParentCell() {
+            return parentCell;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            CellWithParent that = (CellWithParent) o;
+            return x == that.x &&
+                    y == that.y &&
+                    Objects.equals(parentCell, that.parentCell);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y, parentCell);
+        }
+    }
+}
---
+++ src/main/java/algorithms/_5matrix/_2graph/_3PrintMaxSumSquareSubMatrixOfGivenSize.java
@@ -0,0 +1,69 @@
+package algorithms._5matrix._2graph;
+
+/*
+    Print maximum sum square sub-matrix of given size
+
+    https://www.geeksforgeeks.org/print-maximum-sum-square-sub-matrix-of-given-size/
+
+    Given an N x N matrix, find a k x k submatrix where k <= N and k >= 1, such that sum of all the elements in submatrix is maximum.
+    The input matrix can contain zero, positive and negative numbers.
+
+    e.g.
+
+    1   2   -1  4
+    -8  -3  4   2
+    3   8   10  -8
+    -4  -1  1   7
+
+    k=3
+
+    answer is
+
+    1    2   -1  4
+       ------------
+    -8 | -3  4   2 |
+    3  | 8   10  -8|
+    -4 | -1  1   7 |
+       -------------
+
+    Simple solution:
+
+    Start from last cell.
+    Calculate sum of K x K matrix from last cell. That becomes your so far max sum subarray.
+    Then do the same for rest of the cells and keep track of max sum subarray.
+    Time Complexity = O((N x N)(K x K))
+
+    Better Solution:
+
+    Count sum of all cells in each row. Takes O(N x N).
+
+    row  sum
+    0 -> 6
+    1 -> -5
+    2 -> 13
+    3 -> 3
+
+    Count sum of all cells in each col. Takes O(N x N).
+
+    col sum
+    0 -> -8
+    1 -> 6
+    2 -> 14
+    3 -> 5
+
+    Count sum of all cells in the matrix. Takes O(N), if you just iterate through one of the above map and take the sum.
+    17
+
+
+    Now, start from last cell (3,3). Submatrix will go till (1,1)
+    This submatrix's sum will be 17 - 0th row's sum - 0th col's sum.
+
+    Likewise, submatrix from (2,2) will go till (0,0).
+    This submatrix's sum will be 17 - 3rd row's sum - 3rd col's sum.
+
+    So, you have to subtract the sum of rows and cols that fall outside the submatrix.
+
+    This process will take O(N x N).
+*/
+public class _3PrintMaxSumSquareSubMatrixOfGivenSize {
+}
